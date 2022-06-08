edb056860942d4df198f0dc6ad627d597e2e1ee1
Leonardo Matos <leomp120894@gmail.com>
2022-06-08 16:45:33 -0300

    chore(release): Properly getting `pwd` to back after each pkg publish [#8]

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -3,6 +3,7 @@
 /* global $, fs, globby, argv */
 
 // await $`npx standard-version`;
+const pwd = (await $`pwd`).stdout.trim();
 const { version } = JSON.parse(fs.readFileSync('package.json'));
 const packages = await globby(['packages/**/package.json', '!**/node_modules']);
 
@@ -12,8 +13,7 @@ for (let i = 0; i < packages.length; i++) {
   pkg.version = version;
   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
   if (argv.publish) {
-    const { stdout } = await $`pwd`;
     await $`cd ${pkgPath.replace('/package.json', '')} && npm publish --access public`;
-    await $`cd ${stdout}`;
+    await $`cd ${pwd}`;
   }
 }
