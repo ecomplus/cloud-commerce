42d26a3a2b7f6e79ec3714a5aefc02149f2fb8c1
Leonardo Matos <leomp120894@gmail.com>
2022-06-08 16:27:03 -0300

    chore(release): Properly await processing each package one by one [#8]

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -1,11 +1,13 @@
 #!/usr/bin/env zx
+/* eslint-disable no-console, no-await-in-loop */
 /* global $, fs, globby, argv */
 
 // await $`npx standard-version`;
 const { version } = JSON.parse(fs.readFileSync('package.json'));
 const packages = await globby(['packages/**/package.json', '!**/node_modules']);
 
-packages.forEach(async (pkgPath) => {
+for (let i = 0; i < packages.length; i++) {
+  const pkgPath = packages[i];
   const pkg = JSON.parse(fs.readFileSync(pkgPath));
   pkg.version = version;
   fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
@@ -14,4 +16,4 @@ packages.forEach(async (pkgPath) => {
     await $`cd ${pkgPath.replace('/package.json', '')} && npm publish --access public`;
     await $`cd ${stdout}`;
   }
-});
+}
