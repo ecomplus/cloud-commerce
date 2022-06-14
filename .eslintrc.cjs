8d168bcd05c3246896fe30709ca670af5113e10e
Leonardo Matos <leomp120894@gmail.com>
2022-06-13 22:00:11 -0300

    chore(eslint): Set target es2020
    Disable rule `no-underscore-dangle`
    
    Rename file to explicit .cjs

---
+++ .eslintrc.cjs
@@ -0,0 +1,66 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+    '@typescript-eslint',
+  ],
+  ignorePatterns: [
+    '**/dist',
+  ],
+  rules: {
+    'no-await-in-loop': 'warn',
+    'no-shadow': 'warn',
+    'no-param-reassign': 'warn',
+    'no-underscore-dangle': 'off',
+    'no-continue': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      { js: 'ignorePackages', ts: 'never' },
+    ],
+    'padded-blocks': [
+      'error',
+      'never',
+    ],
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: ['directive', 'import', 'export'], next: '*' },
+      { blankLine: 'always', prev: '*', next: ['directive', 'export'] },
+      { blankLine: 'never', prev: 'directive', next: 'directive' },
+      { blankLine: 'never', prev: 'import', next: 'import' },
+    ],
+    'arrow-body-style': 'off',
+    'import/order': [
+      'error',
+      { groups: ['type', 'builtin', 'external', 'parent', 'sibling', 'index'] },
+    ],
+    'no-plusplus': [
+      'error',
+      { allowForLoopAfterthoughts: true },
+    ],
+    'no-multi-spaces': [
+      'error',
+      { ignoreEOLComments: true },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+};
