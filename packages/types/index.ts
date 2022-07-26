57c207cb6f7af3c8c0007173cc49859e4ecba1f3
Leonardo Matos <leomp120894@gmail.com>
2022-07-26 18:05:40 -0300

    feat(types): Also exporting modules params and responses type interfaces

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -15,6 +15,15 @@ import type {
   ResourceListResult,
   EventsResult,
 } from '@cloudcommerce/api/src/types';
+import type { ApplyDiscountParams } from '@cloudcommerce/modules/schemas/types/apply_discount:params';
+import type { ApplyDiscountResponse } from '@cloudcommerce/modules/schemas/types/apply_discount:response';
+import type { CalculateShippingParams } from '@cloudcommerce/modules/schemas/types/calculate_shipping:params';
+import type { CalculateShippingResponse } from '@cloudcommerce/modules/schemas/types/calculate_shipping:response';
+import type { ListPaymentsParams } from '@cloudcommerce/modules/schemas/types/list_payments:params';
+import type { ListPaymentsResponse } from '@cloudcommerce/modules/schemas/types/list_payments:response';
+import type { CreateTransactionParams } from '@cloudcommerce/modules/schemas/types/create_transaction:params';
+import type { CreateTransactionResponse } from '@cloudcommerce/modules/schemas/types/create_transaction:response';
+import type { CheckoutBody } from '@cloudcommerce/modules/schemas/types/@checkout:params';
 
 type AppEventsTopic = 'orders-new'
   | 'orders-setAnyStatus'
@@ -45,4 +54,13 @@ export type {
   ResourceListResult,
   EventsResult,
   AppEventsTopic,
+  ApplyDiscountParams,
+  ApplyDiscountResponse,
+  CalculateShippingParams,
+  CalculateShippingResponse,
+  ListPaymentsParams,
+  ListPaymentsResponse,
+  CreateTransactionParams,
+  CreateTransactionResponse,
+  CheckoutBody,
 };
