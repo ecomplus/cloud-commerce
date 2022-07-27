489155ba29c3ba58a158bf4183f3a48be4b14489
Leonardo Matos <leomp120894@gmail.com>
2022-07-27 00:08:17 -0300

    build: Fix `build-lib.sh` to set .js extension on relative imports
    Keep fixing duplicated .cjs.js

--- scripts/build-lib.sh
+++ scripts/build-lib.sh
@@ -2,5 +2,6 @@
 
 rm -rf lib
 npx tsc --outDir lib
-find lib -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/(import [^']+)'(\\.[^'.]+)';/\\1'\\2.js';/g"
+find lib -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/(import [^']+)'(\\.[^']+)';/\\1'\\2.js';/g"
+find lib -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/.([cm])js.js/.\\1js/g"
 npx eslint --rule 'max-len: off' --ext .js lib --fix
