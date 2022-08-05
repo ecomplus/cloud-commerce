6853a00ebca50df86f773320832ce60ab4410da4
Leonardo Matos <leomp120894@gmail.com>
2022-08-05 20:53:17 -0300

    chore(discounts): Debug apply discount module function called

--- packages/apps/discounts/src/index.ts
+++ packages/apps/discounts/src/index.ts
@@ -1,7 +1,9 @@
 import type { AppEventsTopic } from '@cloudcommerce/types';
+import { logger } from 'firebase-functions';
 
 export const events: AppEventsTopic[] = [];
 
 export const applyDiscount = async () => {
+  logger.info('Hello discounts app');
   return {};
 };
