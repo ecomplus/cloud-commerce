3a4ae3996066ece4c502d55b39f79f9fab20fee3
Leonardo Matos <leomp120894@gmail.com>
2022-08-05 02:13:29 -0300

    feat(events): Setup `@cloudcommerce/events` pkg with first listener start [#29]

---
+++ packages/events/src/firebase.ts
@@ -0,0 +1,30 @@
+/* eslint-disable import/prefer-default-export */
+
+import type { AppEventsTopic } from '@cloudcommerce/types';
+import 'source-map-support/register.js';
+// eslint-disable-next-line import/no-unresolved
+import { initializeApp } from 'firebase-admin/app';
+// eslint-disable-next-line import/no-unresolved
+import { runWith, logger } from 'firebase-functions';
+
+initializeApp();
+
+const eventMaxAgeMs = 60000;
+const newOrderTopic: AppEventsTopic = 'orders-new';
+
+export const onNewOrder = runWith({ failurePolicy: true })
+  .pubsub.topic(newOrderTopic).onPublish((message, context) => {
+    const eventAgeMs = Date.now() - Date.parse(context.timestamp);
+    if (eventAgeMs > eventMaxAgeMs) {
+      logger.warn(`Dropping event ${context.eventId} with age[ms]: ${eventAgeMs}`);
+      return;
+    }
+    // Hide API key for security
+    process.env.ECOM_API_KEY = '***';
+    const { json } = message;
+    logger.info({
+      topic: newOrderTopic,
+      eventId: context.eventId,
+      json,
+    });
+  });
