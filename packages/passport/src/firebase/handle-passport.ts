ac2b0b88645b45dfa2c2d3dda49832b15537e711
Leonardo Matos <leomp120894@gmail.com>
2022-08-16 16:38:48 -0300

    fix(passport): Consider valid tokens stored with at least 2 min to expire
    Clean code, remove unecessary `X-Store-ID` header and `readStore` method

--- packages/passport/src/firebase/handle-passport.ts
+++ packages/passport/src/firebase/handle-passport.ts
@@ -1,47 +1,17 @@
 import type { Response } from 'firebase-functions';
 import type { Customers } from '@cloudcommerce/types';
-import type { Firestore, DocumentReference } from 'firebase-admin/firestore';
+import type { Firestore } from 'firebase-admin/firestore';
 // eslint-disable-next-line import/no-unresolved
 import { Auth } from 'firebase-admin/auth';
 import { logger } from 'firebase-functions';
-import config from '@cloudcommerce/firebase/lib/config';
 import api from '@cloudcommerce/api';
-
-const firestoreColl = 'apiToken';
-const { storeId } = config.get();
-
-const readStore = async (
-  apiAuth: { authenticationId: string, apiKey: string },
-) => {
-  const headers = {
-    'X-Store-ID': `${storeId}`,
-  };
-
-  try {
-    const { data } = await api.get('stores/me', {
-      ...apiAuth,
-      headers,
-    });
-    return data;
-  } catch (e) {
-    logger.error(e);
-    return null;
-  }
-};
 
 const findCustomerByEmail = async (
   email: string | undefined,
   apiAuth: { authenticationId: string, apiKey: string },
 ) => {
-  const headers = {
-    'X-Store-ID': `${storeId}`,
-  };
-
   try {
-    const { data } = await api.get(`customers?main_email=${email}`, {
-      ...apiAuth,
-      headers,
-    });
+    const { data } = await api.get(`customers?main_email=${email}`, apiAuth);
     if (data.result.length) {
       return data.result[0];
     }
@@ -68,6 +38,93 @@ const checkFirebaseAuth = async (
   }
 };
 
+const createCustomer = async (
+  customer: Customers,
+  apiAuth: { authenticationId: string, apiKey: string },
+) => {
+  try {
+    const { data } = await api.post('customers', customer, apiAuth);
+    return data._id;
+  } catch (e) {
+    logger.error(e);
+    return null;
+  }
+};
+
+const generateAccessToken = async (
+  firestore: Firestore,
+  customerId: string,
+  apiAuth: { authenticationId: string, apiKey: string },
+): Promise<null | {
+  customer_id: string,
+  access_token: string,
+  expires: string,
+}> => {
+  const docRef = firestore.doc(`customerTokens/${customerId}`);
+  const doc = await docRef.get();
+  const expires: string | undefined = doc.data()?.expires;
+  if (expires && new Date(expires).getTime() - Date.now() >= 2 * 60 * 1000) {
+    return doc.data() as { customer_id: string, access_token: string, expires: string };
+  }
+  try {
+    const { data } = await api({
+      endpoint: 'authenticate',
+      method: 'post',
+      body: {
+        _id: apiAuth.authenticationId,
+        api_key: apiAuth.apiKey,
+        customer_id: customerId,
+      },
+    });
+    const accessToken = {
+      access_token: data.access_token,
+      expires: data.expires,
+      customer_id: customerId,
+    };
+    docRef.set(accessToken).catch(logger.error);
+    return accessToken;
+  } catch (e) {
+    logger.error(e);
+    return null;
+  }
+};
+
+const getAuthCustomerApi = async (
+  firestore: Firestore,
+  apiAuth: { authenticationId: string, apiKey: string },
+  authtoken: string | string[] | undefined,
+  authFirebase: Auth,
+) => {
+  const customerFirebaseAuth = await checkFirebaseAuth(authFirebase, authtoken);
+  if (customerFirebaseAuth !== null && customerFirebaseAuth.email) {
+    const customer = await findCustomerByEmail(
+      customerFirebaseAuth.email,
+      apiAuth,
+    );
+    if (customer !== null) {
+      return generateAccessToken(firestore, customer._id, apiAuth);
+    }
+    const newCustomer = {
+      display_name: customerFirebaseAuth.name || '',
+      main_email: customerFirebaseAuth.email,
+      emails: [{
+        address: customerFirebaseAuth.email,
+        verified: customerFirebaseAuth.email_verified,
+      }],
+      oauth_providers: [{
+        provider: customerFirebaseAuth.firebase.sign_in_provider,
+        user_id: customerFirebaseAuth.user_id,
+      }],
+    } as Customers;
+    const customerId = await createCustomer(newCustomer, apiAuth);
+    if (customerId) {
+      return generateAccessToken(firestore, customerId, apiAuth);
+    }
+  }
+  // TODO: Find customer by phone number, generate token if found, otherwise unauthorize
+  return null;
+};
+
 const sendError = (
   res: Response,
   msg?: string,
@@ -86,139 +143,7 @@ const sendError = (
   }
 };
 
-const gerateAcessTokenCustomerApi = async (
-  documentRef: DocumentReference,
-  customerId: string,
-  apiAuth: { authenticationId: string, apiKey: string },
-) => {
-  const headers = {
-    'X-Store-ID': `${storeId}`,
-  };
-
-  try {
-    const { data } = await api.post(
-      'authenticate',
-      {
-        _id: apiAuth.authenticationId,
-        api_key: apiAuth.apiKey,
-        customer_id: customerId,
-      },
-      { headers },
-    );
-    if (documentRef) {
-      documentRef.set({ ...data, customer_id: customerId }).catch(logger.error);
-    }
-    return data;
-  } catch (e) {
-    logger.error(e);
-    return null;
-  }
-};
-
-const checkAcessTokenCustomerApi = async (
-  firestore:Firestore,
-  customerId: string,
-  apiAuth: { authenticationId: string, apiKey: string },
-) => {
-  const docRef = firestore.doc(`${firestoreColl}/${customerId}`);
-  if (docRef) {
-    // create auth in api
-    const doc = await docRef.get();
-    const expires = doc.data()?.expires;
-    if (doc.exists && expires
-      && (Date.now() <= (new Date(expires).getTime() - 10 * 60 * 1000))) {
-      return doc.data();
-    }
-    return gerateAcessTokenCustomerApi(docRef, customerId, apiAuth);
-  }
-  return null;
-};
-
-const createCustomer = async (
-  customer: Customers,
-  apiAuth: { authenticationId: string, apiKey: string },
-) => {
-  const headers = {
-    'X-Store-ID': `${storeId}`,
-  };
-
-  try {
-    const { data } = await api.post(
-      'customers',
-      customer,
-      {
-        ...apiAuth,
-        headers,
-      },
-    );
-    return data._id;
-  } catch (e) {
-    logger.error(e);
-    return null;
-  }
-};
-
-const gerateAuthCustomerApi = async (
-  firestore: Firestore,
-  customerId: string,
-  apiAuth: { authenticationId: string, apiKey: string },
-) => {
-  const customerAuthApi = await checkAcessTokenCustomerApi(
-    firestore,
-    customerId,
-    apiAuth,
-  );
-  if (customerAuthApi) {
-    customerAuthApi.customer_id = customerId;
-    delete customerAuthApi.my_id;
-    return customerAuthApi;
-  }
-  logger.error(new Error('Acess token api not found'));
-  return null;
-};
-
-const getAuthCustomerApi = async (
-  firestore: Firestore,
-  apiAuth: { authenticationId: string, apiKey: string },
-  authtoken: string | string[] | undefined,
-  authFirebase: Auth,
-) => {
-  const customerFirebaseAuth = await checkFirebaseAuth(authFirebase, authtoken);
-  if (customerFirebaseAuth !== null && customerFirebaseAuth.email) {
-    const customer = await findCustomerByEmail(
-      customerFirebaseAuth.email,
-      apiAuth,
-    );
-    if (customer != null) {
-      // check acess token and return custumer _id and acess token
-      return gerateAuthCustomerApi(firestore, customer._id, apiAuth);
-    }
-    // account not found
-    // create customer in API, checke authentication in api,
-    // return custummer_id and acess token
-    const newCustomer = {
-      display_name: customerFirebaseAuth.name || '',
-      main_email: customerFirebaseAuth.email,
-      emails: [{
-        address: customerFirebaseAuth.email,
-        verified: customerFirebaseAuth.email_verified,
-      }],
-      oauth_providers: [{
-        provider: customerFirebaseAuth.firebase.sign_in_provider,
-        user_id: customerFirebaseAuth.user_id,
-      }],
-    } as Customers;
-    const customerId = await createCustomer(newCustomer, apiAuth);
-    if (customerId) {
-      return gerateAuthCustomerApi(firestore, customerId, apiAuth);
-    }
-  }
-  // TODO: find customer by phone number, if find gerateAuthCustumerApi, otherwise not authorized
-  return null;
-};
-
 export {
-  readStore,
   sendError,
   getAuthCustomerApi,
 };
