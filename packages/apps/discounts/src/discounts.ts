16bbeb50b5c4f01b8528d78eb2b647fd0506c825
Leonardo Matos <leomp120894@gmail.com>
2022-08-17 20:53:38 -0300

    chore(discounts): Debug module request body

--- packages/apps/discounts/src/discounts.ts
+++ packages/apps/discounts/src/discounts.ts
@@ -1,7 +1,8 @@
 /* eslint-disable import/prefer-default-export */
+import type { AppModuleBody } from '@cloudcommerce/types';
 import { logger } from 'firebase-functions';
 
-export const applyDiscount = async () => {
-  logger.info('Hello discounts app');
+export const applyDiscount = async (modBody: AppModuleBody) => {
+  logger.info(modBody);
   return {};
 };
