560845271078a568db9175fb2f1d6e478074880d
Leonardo Matos <leomp120894@gmail.com>
2022-08-30 22:39:05 -0300

    build: Fix regex to properly set .js extensions to imports on lib

--- scripts/build-lib.sh
+++ scripts/build-lib.sh
@@ -2,6 +2,6 @@
 
 rm -rf lib
 npx tsc --outDir lib
-find lib -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/(import [^']+)'(\\.[^']+)';/\\1'\\2.js';/g"
+find lib -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/(import [^']+?)'(\\.[^']+)';/\\1'\\2.js';/g"
 find lib -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/.([cm])js.js/.\\1js/g"
 npx eslint --rule 'max-len: off' --rule 'import/prefer-default-export: off' --ext .js lib --fix
