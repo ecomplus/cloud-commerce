0370347fad1e03e0f5cb2bf9067dd8f438d8dd45
Leonardo Matos <leomp120894@gmail.com>
2022-09-06 18:43:53 -0300

    feat(ssr): Setup SSR handler from `@ecomplus/storefron-renderer`
    Astro compiled server handler from `global.ssr_handler`
    
    Based on https://github.com/ecomplus/storefront/blob/master/%40ecomplus/storefront-renderer/functions/index.js and https://github.com/withastro/astro/blob/main/examples/ssr/server/server.mjs

--- packages/ssr/src/firebase.ts
+++ packages/ssr/src/firebase.ts
@@ -3,9 +3,10 @@ import '@cloudcommerce/firebase/lib/init';
 // eslint-disable-next-line import/no-unresolved
 import { onRequest } from 'firebase-functions/v2/https';
 import config from '@cloudcommerce/firebase/lib/config';
+import serveStorefront from './firebase/serve-storefront';
 
-const options = config.get().httpsFunctionOptions;
+const { httpsFunctionOptions } = config.get();
 
-export const ssr = onRequest(options, (request, response) => {
-  response.send('<h1>Hello SSR!</h1>');
+export const ssr = onRequest(httpsFunctionOptions, (req, res) => {
+  serveStorefront(req, res);
 });
---
+++ packages/ssr/src/firebase/serve-storefront.ts
@@ -0,0 +1,78 @@
+import type { Request, Response } from 'firebase-functions';
+import { join as joinPath } from 'path';
+import { readFile } from 'fs/promises';
+
+const {
+  STOREFRONT_BASE_DIR,
+  STOREFRONT_LONG_CACHE,
+} = process.env;
+
+const baseDir = STOREFRONT_BASE_DIR || process.cwd();
+const clientRoot = new URL(joinPath(baseDir, 'dist/client/'), import.meta.url);
+const isLongCache = String(STOREFRONT_LONG_CACHE).toLowerCase() === 'true';
+
+export default (req: Request, res: Response) => {
+  const url = req.url.replace(/\?.*$/, '').replace(/\.html$/, '');
+
+  const setStatusAndCache = (status: number, defaultCache: string) => {
+    return res.status(status)
+      .set('X-SSR-ID', `v1/${Math.random()}`)
+      .set(
+        'Cache-Control',
+        (typeof global.cache_control === 'function' && global.cache_control(status))
+          || defaultCache,
+      );
+  };
+
+  const redirect = (toUrl: string, status = 302) => {
+    let sMaxAge = status === 301 ? 360 : 12;
+    if (isLongCache) {
+      sMaxAge *= 10;
+    }
+    let cacheControl = `public, max-age=30, s-maxage=${sMaxAge}`;
+    if (status === 302) {
+      cacheControl += ', proxy-revalidate';
+    }
+    setStatusAndCache(status, cacheControl)
+      .set('Location', toUrl).end();
+  };
+
+  const fallback = (status = 404) => {
+    if (url.slice(-1) === '/') {
+      redirect(url.slice(0, -1));
+    } else if (url !== '/404' && (/\/[^/.]+$/.test(url) || /\.x?html$/.test(url))) {
+      setStatusAndCache(status, `public, max-age=${(isLongCache ? 120 : 30)}`)
+        .send('<html><head>'
+          + `<meta http-equiv="refresh" content="0; url=/404?url=${encodeURIComponent(url)}"/>`
+          + '</head><body></body></html>');
+    } else {
+      setStatusAndCache(status, isLongCache
+        ? 'public, max-age=60, s-maxage=86400'
+        : 'public, max-age=60, s-maxage=300')
+        .end();
+    }
+  };
+
+  /*
+  https://github.com/withastro/astro/blob/main/examples/ssr/server/server.mjs
+  import { handler as ssrHandler } from '../dist/server/entry.mjs';
+  global.ssr_handler = ssrHandler;
+  */
+  global.ssr_handler(req, res, async (err: any) => {
+    if (err) {
+      res.set('X-SSR-ERROR', err.stack);
+      fallback(500);
+      return;
+    }
+    const local = new URL(`.${url}`, clientRoot);
+    try {
+      const data = await readFile(local);
+      setStatusAndCache(200, isLongCache
+        ? 'public, max-age=60, s-maxage=604800'
+        : 'public, max-age=60, s-maxage=600, stale-while-revalidate=2592000')
+        .send(data);
+    } catch {
+      fallback();
+    }
+  });
+};
