ba87901bea990b13fa154731739777c277df9a4c
Wisley Alves <wisley7lagoas@yahoo.com.br>
2022-09-14 18:09:10 -0300

    refactor(functions-checkout): Create function to add new order with customer authentication

---
+++ packages/modules/src/firebase/functions-checkout/new-order.ts
@@ -0,0 +1,44 @@
+import type { Orders } from '@cloudcommerce/types';
+import { logger } from 'firebase-functions';
+import api from '@cloudcommerce/api';
+
+type BodyOrder = Omit<Orders, '_id' | 'created_at' | 'updated_at' | 'store_id' >
+
+export default async (
+  orderBody:BodyOrder,
+  acessToken:string,
+) => {
+  try {
+    const orderId = (await api.post(
+      'orders',
+      orderBody,
+      {
+        headers: {
+          Authentication: `Bearer ${acessToken}`,
+        },
+      },
+    )).data._id;
+
+    return new Promise<Orders|null>((resolve) => {
+      setTimeout(async () => {
+        try {
+          const order = (await api.get(
+            `orders/${orderId}`,
+            {
+              headers: {
+                Authentication: `Bearer ${acessToken}`,
+              },
+            },
+          )).data;
+          resolve(order);
+        } catch (e) {
+          logger.error(e);
+          resolve(null);
+        }
+      }, 800);
+    });
+  } catch (e) {
+    logger.error(e);
+    return null;
+  }
+};
