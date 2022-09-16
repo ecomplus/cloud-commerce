714c8b3f502082a6a395b40e7a792540cd9af861
Leonardo Matos <leomp120894@gmail.com>
2022-09-16 19:49:11 -0300

    fix(ssr): Use `compression` pkg (middleware) to gzip
    Try to fix untraced SSR error "upstream connect error or disconnect/reset before headers. reset reason: protocol error"

--- packages/ssr/src/firebase/serve-storefront.ts
+++ packages/ssr/src/firebase/serve-storefront.ts
@@ -1,29 +1,24 @@
 import type { Request, Response } from 'firebase-functions';
-import type { OutgoingHttpHeaders, OutgoingHttpHeader } from 'http';
 import { join as joinPath } from 'path';
 import { readFile } from 'fs/promises';
-import { gzip } from 'zlib';
-import logger from 'firebase-functions/lib/logger';
+import compression from 'compression';
 
 const { STOREFRONT_BASE_DIR } = process.env;
 const baseDir = STOREFRONT_BASE_DIR || process.cwd();
 const clientRoot = new URL(joinPath(baseDir, 'dist/client/'), import.meta.url);
+const compress = compression();
 
-type InterceptedResponse = Response & {
-  WRITEHEAD?: Response['writeHead'],
-  WRITE?: Response['write'],
-};
-
-export default (req: Request, res: InterceptedResponse) => {
-  logger.info('Serving storefront');
+export default (req: Request, res: Response) => {
   const url = req.url.replace(/\?.*$/, '').replace(/\.html$/, '');
 
   const setStatusAndCache = (status: number, defaultCache: string) => {
-    return res.status(status).set(
-      'Cache-Control',
-      (typeof global.cache_control === 'function' && global.cache_control(status))
-        || defaultCache,
-    );
+    return res.status(status)
+      .set('X-SSR-ID', `v1/${Date.now()}`)
+      .set(
+        'Cache-Control',
+        (typeof global.cache_control === 'function' && global.cache_control(status))
+          || defaultCache,
+      );
   };
 
   const fallback = (err: any, status = 500) => {
@@ -39,53 +34,26 @@ export default (req: Request, res: InterceptedResponse) => {
     }
   };
 
-  res.WRITEHEAD = res.writeHead;
-  res.writeHead = (
-    statusCode: number,
-    headers?: string | OutgoingHttpHeaders | OutgoingHttpHeader[],
-  ) => {
-    if (headers && typeof headers === 'object' && !Array.isArray(headers)) {
-      delete headers['transfer-encoding'];
-      headers['Transfer-Encoding'] = 'gzip, chunked';
-      headers['X-SSR-ID'] = `v1/${Date.now()}`;
-    }
-    return (res.WRITEHEAD as Response['writeHead'])(
-      statusCode,
-      headers as OutgoingHttpHeaders,
-    );
-  };
-
-  res.WRITE = res.write;
-  res.write = (chunk: any) => {
-    gzip(chunk, (err, data) => {
+  compress(req, res, async () => {
+    /*
+    https://github.com/withastro/astro/blob/main/examples/ssr/server/server.mjs
+    import { handler as ssrHandler } from '../dist/server/entry.mjs';
+    global.ssr_handler = ssrHandler;
+    */
+    global.ssr_handler(req, res, async (err: any) => {
       if (err) {
-        logger.error(err);
+        res.set('X-SSR-Error', err.message);
         fallback(err);
-      } else {
-        (res.WRITE as Response['write'])(data);
+        return;
+      }
+      const local = new URL(`.${url}`, clientRoot);
+      try {
+        const data = await readFile(local);
+        setStatusAndCache(200, 'public, max-age=60, s-maxage=600')
+          .send(data);
+      } catch {
+        fallback(err, 404);
       }
     });
-    return true;
-  };
-
-  /*
-  https://github.com/withastro/astro/blob/main/examples/ssr/server/server.mjs
-  import { handler as ssrHandler } from '../dist/server/entry.mjs';
-  global.ssr_handler = ssrHandler;
-  */
-  global.ssr_handler(req, res, async (err: any) => {
-    if (err) {
-      res.set('X-SSR-Error', err.message);
-      fallback(err);
-      return;
-    }
-    const local = new URL(`.${url}`, clientRoot);
-    try {
-      const data = await readFile(local);
-      setStatusAndCache(200, 'public, max-age=60, s-maxage=600')
-        .send(data);
-    } catch {
-      fallback(err, 404);
-    }
   });
 };
