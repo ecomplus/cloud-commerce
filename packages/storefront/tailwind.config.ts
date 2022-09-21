037f3e8add9002d93622abdea7541357a1326588
Leonardo Matos <leomp120894@gmail.com>
2022-09-21 15:32:53 -0300

    chore(storefront): Edit Tailwind config to extend colors with `surface` and `muted`

--- packages/storefront/tailwind.config.ts
+++ packages/storefront/tailwind.config.ts
@@ -6,19 +6,28 @@ const genTailwindConfig = ({
 } = {}) => ({
   theme: {
     extend: {
-      colors: ['primary', 'secondary', 'contrast'].reduce((colors, color) => {
-        const colorVariations = ['hover', 'focus', 'inverse'];
-        if (color !== 'contrast') {
-          colorVariations.push(...colorVariants);
-        }
-        colors[color] = colorVariations.reduce((colorPalette, variant) => {
-          colorPalette[variant] = `var(--${color}-${variant})`;
-          return colorPalette;
-        }, {
-          DEFAULT: `var(--${color})`,
-        });
-        return colors;
-      }, {}),
+      colors: {
+        ...['primary', 'secondary', 'contrast'].reduce((colors, color) => {
+          const colorVariations = ['hover', 'focus', 'inverse'];
+          if (color !== 'contrast') {
+            colorVariations.push(...colorVariants);
+          }
+          colors[color] = colorVariations.reduce((colorPalette, variant) => {
+            colorPalette[variant] = `var(--${color}-${variant})`;
+            return colorPalette;
+          }, {
+            DEFAULT: `var(--${color})`,
+          });
+          return colors;
+        }, {}),
+        ...['surface', 'muted'].reduce((colors, color) => ({
+          [color]: {
+            DEFAULT: 'var(--muted-color)',
+            border: 'var(--muted-border-color)',
+          },
+          ...colors,
+        }), {}),
+      },
     },
   },
 });
