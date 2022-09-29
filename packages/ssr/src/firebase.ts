c795e90b2e8b9f463d571763e19b9f4508b5fd1a
Leonardo Matos <leomp120894@gmail.com>
2022-09-28 21:33:48 -0300

    feat(ssr): Getting deploy options from config `ssrFunctionOptions`
    New `feeds` HTTPs v2 function to handle sitemaps/XMLs

--- packages/ssr/src/firebase.ts
+++ packages/ssr/src/firebase.ts
@@ -8,19 +8,16 @@ import { onRequest } from 'firebase-functions/v2/https';
 import config from '@cloudcommerce/firebase/lib/config';
 import serveStorefront from './firebase/serve-storefront';
 
-const {
-  httpsFunctionOptions: {
-    timeoutSeconds,
-    memory,
-    minInstances,
-  },
-} = config.get();
+const { httpsFunctionOptions, ssrFunctionOptions } = config.get();
 
-export const ssr = onRequest({
-  region: 'us-central1',
-  timeoutSeconds: timeoutSeconds || 30,
-  memory: memory || '512MiB',
-  minInstances: typeof minInstances === 'number' ? minInstances : 1,
+export const ssr = onRequest(ssrFunctionOptions, (req, res) => {
+  serveStorefront(req, res);
+});
+
+export const feeds = onRequest({
+  ...httpsFunctionOptions,
+  timeoutSeconds: 120,
+  memory: ssrFunctionOptions.memory,
 }, (req, res) => {
   serveStorefront(req, res);
 });
