b354b6c53bbfdeec5ce8d2452ca70c5ef28232d9
Wisley Alves <wisley7lagoas@yahoo.com.br>
2022-10-05 14:10:28 -0300

    refactor(script): Handle minification file

---
+++ scripts/script-minification.mjs
@@ -0,0 +1,27 @@
+import { resolve } from 'path';
+import {
+  readdirSync,
+  readFileSync,
+  writeFileSync,
+} from 'fs';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { minify } from 'uglify-js';
+
+const __dirname = process.cwd();
+
+const publicPath = resolve(__dirname, 'assets');
+readdirSync(publicPath).forEach((file) => {
+  if (file.endsWith('.js') && !file.endsWith('.min.js')) {
+    const filePath = resolve(publicPath, file);
+    if (filePath) {
+      const minifyFile = minify(readFileSync(filePath, 'utf8'));
+      if (minifyFile.code) {
+        const outputPath = filePath.replace(/\/([^/]+).js$/, '/$1.min.js');
+        writeFileSync(outputPath, minifyFile.code, 'utf8');
+      } else {
+        // eslint-disable-next-line no-console
+        console.error(minifyFile);
+      }
+    }
+  }
+});
