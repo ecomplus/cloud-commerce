df608792b04a01e5c945bb4f455caee0555ffecc
Leonardo Matos <leomp120894@gmail.com>
2022-10-12 18:30:06 -0300

    chore(storefront): Set `src/{pages,layouts,...}` with symlink from Store SSR instead

--- packages/storefront/scripts/prepare-monorepo.sh
+++ packages/storefront/scripts/prepare-monorepo.sh
@@ -2,3 +2,8 @@
 
 rm -rf content && ln -s ../../store/functions/ssr/content content
 rm -rf public && ln -s ../../store/functions/ssr/public public
+rm -rf src/pages && ln -s ../../../store/functions/ssr/src/pages src/pages
+rm -rf src/layouts && ln -s ../../../store/functions/ssr/src/layouts src/layouts
+rm -rf src/components && ln -s ../../../store/functions/ssr/src/components src/components
+rm -rf src/scripts && ln -s ../../../store/functions/ssr/src/scripts src/scripts
+rm -rf src/assets && ln -s ../../../store/functions/ssr/src/assets src/assets
