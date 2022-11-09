9a5cff36adf9adff805997abe3d7f66f1d52507b
Leonardo Matos <leomp120894@gmail.com>
2022-11-09 00:09:26 -0300

    build(release): Save `@cloudcommerce/types` as dev dep on SSR codebase (TS support)

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -58,6 +58,7 @@ if (argv.publish) {
             await $`npm i --save @cloudcommerce/api@${version}`;
             await $`npm i --save-dev @cloudcommerce/storefront@${version}`;
             await $`npm i --save-dev @cloudcommerce/i18n@${version}`;
+            await $`npm i --save-dev @cloudcommerce/types@${version}`;
             for (let i = 0; i < astroPkgs.length; i++) {
               const astroPkg = astroPkgs[i];
               const dep = `${astroPkg}@${ssrDependencies[astroPkg]}`;
