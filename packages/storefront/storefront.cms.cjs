1e5e238bae75a17ec0239ebc134b0239314ac944
Leonardo Matos <leomp120894@gmail.com>
2022-11-25 22:53:58 -0300

    chore(storefront): Add CJS version `storefront.cms.cjs`

---
+++ packages/storefront/storefront.cms.cjs
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const { resolve: resolvePath } = require('path');
+
+module.exports = () => {
+  const { STOREFRONT_BASE_DIR } = process.env;
+  let baseDir;
+  if (STOREFRONT_BASE_DIR) {
+    baseDir = resolvePath(process.cwd(), STOREFRONT_BASE_DIR);
+  } else {
+    baseDir = process.cwd();
+  }
+  process.env.STOREFRONT_BASE_DIR = baseDir;
+  const dirContent = resolvePath(baseDir, 'content');
+
+  const cms = (filename) => {
+    if (filename.endsWith('/')) {
+      const dirColl = resolvePath(dirContent, filename);
+      return fs.readdirSync(dirColl).map((_filename) => _filename.replace('.json', ''));
+    }
+    const filepath = resolvePath(dirContent, `${filename}.json`);
+    return JSON.parse(fs.readFileSync(filepath, 'utf8'));
+  };
+
+  let settings;
+  try {
+    settings = cms('settings');
+  } catch (e) {
+    settings = {};
+  }
+  const { domain } = settings;
+  const primaryColor = settings.primary_color || '#137c5c';
+  const secondaryColor = settings.secondary_color || primaryColor;
+
+  return {
+    domain,
+    primaryColor,
+    secondaryColor,
+    settings,
+    cms,
+  };
+};
