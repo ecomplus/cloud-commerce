59153777a762e55e631571868fc3084efcca6353
Leonardo Matos <leomp120894@gmail.com>
2022-12-06 13:19:12 -0300

    build(release): Commit auto generated files after build/test

--- scripts/version-and-release.sh
+++ scripts/version-and-release.sh
@@ -2,6 +2,7 @@
 
 pnpm build --force || exit 1
 pnpm test || exit 1
+git add packages/**/auto-*.d.ts packages/**/.*-auto-* packages/storefront/components.d.ts
 pnpm run -r prerelease
 npx standard-version --commit-all
 npx zx scripts/release.mjs --publish --skip-stores
