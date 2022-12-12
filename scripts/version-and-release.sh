6eae209e19f49604bc55be61e02a591705337454
Leonardo Matos <leomp120894@gmail.com>
2022-12-12 17:34:26 -0300

    build(release): Reverts commit to auto generated types (removed)
    Reverts https://github.com/ecomplus/cloud-commerce/commit/59153777a762e55e631571868fc3084efcca6353

--- scripts/version-and-release.sh
+++ scripts/version-and-release.sh
@@ -2,7 +2,6 @@
 
 pnpm build --force || exit 1
 pnpm test || exit 1
-git add packages/**/auto-*.d.ts packages/**/.*-auto-* packages/storefront/components.d.ts
 pnpm run -r prerelease
 npx standard-version --commit-all
 npx zx scripts/release.mjs --publish --skip-stores
