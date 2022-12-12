eec5e399ed30180bee5180ff5ef26f9253696e9b
Leonardo Matos <leomp120894@gmail.com>
2022-12-11 22:16:10 -0300

    feat(storefront): Starting `Price` component with headless approach
    Using child components with `parent` and typed `Exposed`

--- packages/storefront/src/components.d.ts
+++ packages/storefront/src/components.d.ts
@@ -8,13 +8,19 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ADrawer: typeof import('./lib/components/ADrawer.vue')['default']
-    APrice: typeof import('./lib/components/atoms/APrice.vue')['default']
     HeaderButtonLink: typeof import('./components/header/HeaderButtonLink.vue')['default']
     HeaderButtons: typeof import('./components/header/HeaderButtons.vue')['default']
     HeaderNav: typeof import('./components/header/HeaderNav.vue')['default']
     HeaderNavLink: typeof import('./components/header/HeaderNavLink.vue')['default']
     LoginDrawer: typeof import('./lib/components/LoginDrawer.vue')['default']
     LoginForm: typeof import('./lib/components/LoginForm.vue')['default']
+    Price: typeof import('./lib/components/Price.vue')['default']
+    PriceCashback: typeof import('./lib/components/Price/PriceCashback.vue')['default']
+    PriceCompare: typeof import('./lib/components/Price/PriceCompare.vue')['default']
+    PriceDiscountLabel: typeof import('./lib/components/Price/PriceDiscountLabel.vue')['default']
+    PriceInstallments: typeof import('./lib/components/Price/PriceInstallments.vue')['default']
+    PriceSale: typeof import('./lib/components/Price/PriceSale.vue')['default']
+    PriceWithDiscount: typeof import('./lib/components/Price/PriceWithDiscount.vue')['default']
     TheHeader: typeof import('./components/TheHeader.vue')['default']
     TopBar: typeof import('./components/TopBar.vue')['default']
   }
---
+++ packages/storefront/src/lib/components/Price/PriceCompare.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { Exposed } from '../Price.vue';
+
+const instance = getCurrentInstance();
+const price = (instance.parent.exposed as Exposed).salePrice;
+</script>
+
+<template>
+  <div class="price-sale">
+    {{ price }}
+  </div>
+</template>
