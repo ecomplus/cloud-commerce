1b97d3bb3cd1753f958274eaf9daa55893262c64
Wisley Alves <wisley7lagoas@yahoo.com.br>
2022-12-16 13:11:32 -0300

    feat(app-loyalty-points): Create app

---
+++ packages/apps/loyalty-points/src/loyalty-list-payments.ts
@@ -0,0 +1,53 @@
+import type { AppModuleBody } from '@cloudcommerce/types';
+import type { ListPaymentsParams } from '@cloudcommerce/types/modules/list_payments:params';
+import type { ListPaymentsResponse } from '@cloudcommerce/types/modules/list_payments:response';
+import getProgramId from './get-program-id';
+
+export default (data: AppModuleBody) => {
+  const { application } = data;
+  const params = data.params as ListPaymentsParams;
+
+  const appData = {
+    ...application.data,
+    ...application.hidden_data,
+  };
+
+  // const { storeId } = req
+  // setup basic required response object
+  const response: ListPaymentsResponse = {
+    payment_gateways: [],
+  };
+
+  const label = params.lang === 'pt_br' || !params.lang
+    ? 'Pontos de fidelidade'
+    : 'Loyalty points';
+
+  response.payment_gateways.push({
+    type: 'payment',
+    payment_method: {
+      code: 'loyalty_points',
+      name: label,
+    },
+    label,
+  });
+
+  if (Array.isArray(appData.programs_rules)) {
+    const pointsPrograms = {};
+    appData.programs_rules.forEach((programRule, index) => {
+      const programId = getProgramId(programRule, index);
+      pointsPrograms[programId] = {
+        name: programRule.name,
+        ratio: programRule.ratio || 1,
+        max_points: programRule.max_points,
+        min_subtotal_to_earn: programRule.min_subtotal_to_earn,
+        earn_percentage: programRule.earn_percentage,
+      };
+    });
+
+    if (Object.keys(pointsPrograms).length) {
+      response.loyalty_points_programs = pointsPrograms;
+    }
+  }
+
+  return response;
+};
