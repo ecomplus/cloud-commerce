9ff310e3c70cbd09bbe074bf0acb5cb05c73429a
Leonardo Matos <leomp120894@gmail.com>
2022-12-18 15:35:10 -0300

    feat(storefront): Update `Fade` component to change el height/width on tarnsition with slide

--- packages/storefront/src/lib/components/globals/Fade.vue
+++ packages/storefront/src/lib/components/globals/Fade.vue
@@ -14,8 +14,8 @@ const props = withDefaults(defineProps<Props>(), {
   isEnterFrom: true,
 });
 const duration = computed(() => {
-  if (props.speed === 'default') return 'var(--transition)';
-  return `var(--transition-${props.speed})`;
+  if (props.speed === 'default') return 'var(--transition, .2s linear)';
+  return `var(--transition-${props.speed}, .4s linear)`;
 });
 const transform = computed(() => {
   switch (props.slide) {
@@ -46,10 +46,92 @@ const enterFromTransform = computed(() => {
 const leaveToTransform = computed(() => {
   return props.isLeaveTo ? transform.value : 'none';
 });
+const isSlideY = computed(() => {
+  return props.slide === 'down' || props.slide === 'up';
+});
+const height = computed(() => {
+  return isSlideY.value ? 0 : 'auto';
+});
+const width = computed(() => {
+  return props.slide && !isSlideY.value ? 0 : 'auto';
+});
+const enterFromHeight = computed(() => {
+  return props.isEnterFrom ? height.value : 'auto';
+});
+const leaveToHeight = computed(() => {
+  return props.isLeaveTo ? height.value : 'auto';
+});
+const enterFromWidth = computed(() => {
+  return props.isEnterFrom ? width.value : 'auto';
+});
+const leaveToWidth = computed(() => {
+  return props.isLeaveTo ? width.value : 'auto';
+});
+const willChange = computed(() => {
+  let properties = 'opacity';
+  if (transform.value !== 'none') properties += ', transform';
+  if (height.value !== 'auto') properties += ', height';
+  if (width.value !== 'auto') properties += ', width';
+  return properties;
+});
+const onEnter = (el: HTMLElement) => {
+  if (props.slide) {
+    if (isSlideY.value) {
+      el.style.width = getComputedStyle(el).width;
+      el.style.height = 'auto';
+    } else {
+      el.style.width = 'auto';
+      el.style.height = getComputedStyle(el).height;
+    }
+    el.style.position = 'absolute';
+    el.style.visibility = 'hidden';
+    // eslint-disable-next-line no-shadow
+    const { width, height } = getComputedStyle(el);
+    el.style.width = isSlideY.value ? null : '0';
+    el.style.height = isSlideY.value ? '0' : null;
+    el.style.position = null;
+    el.style.visibility = null;
+    // Force repaint to make sure the animation is triggered correctly
+    // eslint-disable-next-line no-unused-expressions
+    getComputedStyle(el)[isSlideY.value ? 'height' : 'width'];
+    requestAnimationFrame(() => {
+      if (isSlideY.value) {
+        el.style.height = height;
+      } else {
+        el.style.width = width;
+      }
+    });
+  }
+};
+const onAfterEnter = (el: HTMLElement) => {
+  if (props.slide) {
+    el.style[isSlideY.value ? 'height' : 'width'] = 'auto';
+  }
+};
+const onLeave = (el: HTMLElement) => {
+  if (props.slide) {
+    if (isSlideY.value) {
+      el.style.height = getComputedStyle(el).height;
+    } else {
+      el.style.width = getComputedStyle(el).width;
+    }
+    const sizeProp = isSlideY.value ? 'height' : 'width';
+    // eslint-disable-next-line no-unused-expressions
+    getComputedStyle(el)[sizeProp];
+    requestAnimationFrame(() => {
+      el.style[sizeProp] = '0';
+    });
+  }
+};
 </script>
 
 <template>
-  <Transition name="sf-fade">
+  <Transition
+    name="sf-fade"
+    @enter="onEnter"
+    @after-enter="onAfterEnter"
+    @leave="onLeave"
+  >
     <slot />
   </Transition>
 </template>
@@ -57,24 +139,33 @@ const leaveToTransform = computed(() => {
 <style>
 .sf-fade-enter-active,
 .sf-fade-leave-active {
-  transition: opacity var(--duration), transform var(--duration);
-}
-.sf-fade-enter-from {
-  opacity: var(--enter-from-opacity);
-  transform: var(--enter-from-transform);
-}
-.sf-fade-leave-to {
-  opacity: var(--leave-to-opacity);
-  transform: var(--leave-to-transform);
+  transition: opacity var(--duration), transform var(--duration),
+    height var(--duration), width var(--duration);
+  overflow: hidden;
 }
 </style>
 
 <style scoped>
-[class*=sf-fade] {
+* {
+  will-change: v-bind(willChange);
+  transform: translateZ(0);
+  backface-visibility: hidden;
+  perspective: 1000px;
+}
+.sf-fade-enter-active,
+.sf-fade-leave-active {
   --duration: v-bind(duration);
-  --enter-from-opacity: v-bind(enterFromOpacity);
-  --leave-to-opacity: v-bind(leaveToOpacity);
-  --enter-from-transform: v-bind(enterFromTransform);
-  --leave-to-transform: v-bind(leaveToTransform);
+}
+.sf-fade-enter-from {
+  opacity: v-bind(enterFromOpacity);
+  transform: v-bind(enterFromTransform);
+  height: v-bind(enterFromHeight);
+  width: v-bind(enterFromWidth);
+}
+.sf-fade-leave-to {
+  opacity: v-bind(leaveToOpacity);
+  transform: v-bind(leaveToTransform);
+  height: v-bind(leaveToHeight);
+  width: v-bind(leaveToWidth);
 }
 </style>
