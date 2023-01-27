88c60fa08c1a55e84ebd0ac27c292f23168271b5
Leonardo Matos <leomp120894@gmail.com>
2023-01-27 20:55:30 -0300

    feat(storefront): Add `slide` and `controls` slot to `PitchBar`

--- packages/storefront/src/lib/components/PitchBar.vue
+++ packages/storefront/src/lib/components/PitchBar.vue
@@ -24,7 +24,7 @@ const countValidSlides = computed(() => {
 </script>
 
 <template>
-  <div data-pitch-bar class="bg-base-100">
+  <div class="bg-base-100" data-pitch-bar>
     <div class="container md:w-2/3 mx-auto px-3 py-1">
       <Carousel :autoplay="countValidSlides > 1 ? 7000 : null">
         <li v-for="(slide, i) in slides" :key="i">
@@ -39,21 +39,25 @@ const countValidSlides = computed(() => {
                 v-if="parsedContents[i]"
                 v-html="parsedContents[i]"
                 class="prose text-sm text-base-800"
+                data-pitch-bar-slide
               ></span>
             </slot>
           </component>
         </li>
         <template #controls>
-          <div
-            v-show="countValidSlides > 1"
-            class="text-xl leading-none text-base-400"
-          >
-            <CarouselControl
-              :direction="-1"
-              class="pr-2 bg-base-100 hover:text-base-700"
-            />
-            <CarouselControl class="pl-2 bg-base-100 hover:text-base-700" />
-          </div>
+          <slot name="controls" v-bind="{ countValidSlides }">
+            <div
+              v-show="countValidSlides > 1"
+              class="text-xl leading-none text-base-400"
+              data-pitch-bar-controls
+            >
+              <CarouselControl
+                :direction="-1"
+                class="pr-2 bg-base-100 hover:text-base-700"
+              />
+              <CarouselControl class="pl-2 bg-base-100 hover:text-base-700" />
+            </div>
+          </slot>
         </template>
       </Carousel>
     </div>
