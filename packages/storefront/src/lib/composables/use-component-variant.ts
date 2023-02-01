ef7ab8f4038a5179fe36a163250b8b25739ae4ce
Leonardo Matos <leomp120894@gmail.com>
2023-02-01 01:48:19 -0300

    fix(storefront): Update `useComponentVariant` composable to ignore `modelValue`
    Add support for hard set string props as second argument

--- packages/storefront/src/lib/composables/use-component-variant.ts
+++ packages/storefront/src/lib/composables/use-component-variant.ts
@@ -1,11 +1,13 @@
 import { computed } from 'vue';
 
-const useComponentVariant = (props: Record<string, any>) => {
+const useComponentVariant = (props: Record<string, any>, stringProps?: string[]) => {
   return computed(() => {
     let variantName = '';
     Object.keys(props).forEach((prop) => {
-      if (props[prop] === true) {
+      if (props[prop] === true && prop !== 'modelValue') {
         variantName += ` ${prop.replace(/^(is|has)/, '')}`;
+      } else if (stringProps && stringProps.includes(prop)) {
+        variantName += ` ${prop}:${props[prop]}`;
       }
     });
     return variantName.slice(1);
