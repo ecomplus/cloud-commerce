b026bc7bd7e12e70b70768fc820ccbf2d88730e6
Leonardo Matos <leomp120894@gmail.com>
2023-02-03 00:04:42 -0300

    fix(storefront): Stop `Drawer` from removing header backdrop by default
    Removing root Teleport and anchor div to keep dialog easily editable
    
    Remove `isTeleport` and add `position` prop

--- packages/storefront/src/lib/components/Drawer.vue
+++ packages/storefront/src/lib/components/Drawer.vue
@@ -10,24 +10,23 @@ import useComponentVariant from '@@sf/composables/use-component-variant';
 export interface Props {
   modelValue?: boolean;
   placement?: 'start' | 'end' | 'top' | 'bottom';
-  isTeleported?: boolean;
+  position?: 'fixed' | 'absolute';
   hasCloseButton?: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
   modelValue: false,
   placement: 'start',
-  isTeleported: false,
+  position: 'fixed',
   hasCloseButton: true,
 });
 const emit = defineEmits([
   'update:modelValue',
 ]);
 const close = () => emit('update:modelValue', false);
-const anchor = ref(null);
-const canvas = ref(null);
+const drawer = ref(null);
 const outsideClickListener = (ev: MouseEvent) => {
-  if (!canvas.value?.contains(ev.target)) {
+  if (!drawer.value?.contains(ev.target)) {
     close();
   }
 };
@@ -37,23 +36,14 @@ const escClickListener = (ev: KeyboardEvent) => {
   }
 };
 watch(toRef(props, 'modelValue'), async (isOpen) => {
-  const header = anchor.value.closest('[class*="backdrop-"]');
   if (isOpen) {
     document.body.style.overflow = 'hidden';
-    if (header) {
-      header.style.backdropFilter = 'none';
-    }
     setTimeout(() => {
       document.addEventListener('click', outsideClickListener, { passive: true });
       document.addEventListener('keydown', escClickListener, { passive: true });
     }, 500);
   } else {
     document.body.style.overflow = null;
-    if (header) {
-      setTimeout(() => {
-        header.style.backdropFilter = null;
-      }, 500);
-    }
     document.removeEventListener('click', outsideClickListener);
     document.removeEventListener('keydown', escClickListener);
   }
@@ -66,38 +56,44 @@ const slideTo = computed(() => {
     default: return 'down';
   }
 });
+const isFixed = computed(() => {
+  return props.position === 'fixed';
+});
+const isPlacementX = computed(() => {
+  return props.placement === 'start' || props.placement === 'end';
+});
 const componentVariant = useComponentVariant(props, ['placement']);
 </script>
 
 <template>
-  <div ref="anchor" class="relative">
-    <Teleport to="#teleported" :disabled="!isTeleported">
-      <Fade :slide="slideTo" speed="slow" is-floating>
-        <dialog
-          v-if="modelValue"
-          class="w-screen max-w-sm p-0 z-50"
-          :class="isTeleported ? 'fixed top-0 left-0' : 'absolute'"
-          :open="modelValue"
-          :data-drawer="componentVariant"
+  <Fade :slide="slideTo" speed="slow" is-floating>
+    <dialog
+      v-if="modelValue"
+      ref="drawer"
+      class="w-screen max-w-sm shadow p-0 m-0 z-50"
+      :class="[
+        position,
+        isFixed ? `top-0 left-0 ${(isPlacementX ? 'h-screen' : '')}` : null,
+      ]"
+      :open="modelValue"
+      :data-drawer="componentVariant"
+    >
+      <div class="relative">
+        <button
+          v-if="hasCloseButton"
+          type="button"
+          :aria-label="$t.i19close"
+          @click.prevent="close"
+          class="absolute top-2"
+          :class="placement === 'end' ? 'left-2' : 'right-2'"
+          data-drawer-close
         >
-          <div ref="canvas" class="relative">
-            <button
-              v-if="hasCloseButton"
-              type="button"
-              :aria-label="$t.i19close"
-              @click.prevent="close"
-              class="absolute top-2"
-              :class="placement === 'end' ? 'left-2' : 'right-2'"
-              data-drawer-close
-            >
-              <slot name="close">
-                <i class="i-close text-base-400 text-3xl"></i>
-              </slot>
-            </button>
-            <slot />
-          </div>
-        </dialog>
-      </Fade>
-    </Teleport>
-  </div>
+          <slot name="close">
+            <i class="i-close text-base-400 text-3xl"></i>
+          </slot>
+        </button>
+        <slot />
+      </div>
+    </dialog>
+  </Fade>
 </template>
