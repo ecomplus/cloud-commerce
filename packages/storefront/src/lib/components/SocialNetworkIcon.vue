9702da68bac461a085c58de143ddf1b5d0c120c7
Leonardo Matos <leomp120894@gmail.com>
2023-02-18 21:29:52 -0300

    feat(storefront): New `SocialNetworkIcon` and `SocialNetworkLink` components
    Handling icon class names with switch for UnoCSS detection
    
    Fixed Whatsapp link for desktop and mobile

---
+++ packages/storefront/src/lib/components/SocialNetworkIcon.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import type { NetworkName } from '@@sf/server-data';
+import { computed } from 'vue';
+
+export interface Props {
+  as?: string;
+  network: NetworkName;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: 'i',
+});
+const iconClassName = computed(() => {
+  switch (props.network) {
+    case 'facebook': return 'i-facebook';
+    case 'instagram': return 'i-instagram';
+    case 'whatsapp': return 'i-whatsapp';
+    case 'youtube': return 'i-youtube';
+    case 'pinterest': return 'i-pinterest';
+    case 'tiktok': return 'i-tiktok';
+    case 'twitter': return 'i-twitter';
+    default: return '';
+  }
+});
+</script>
+
+<template>
+  <component :is="as" :class="iconClassName" />
+</template>
---
+++ packages/storefront/src/lib/components/SocialNetworkLink.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { NetworkName } from '@@sf/server-data';
+import { computed } from 'vue';
+import { isMobile } from '@@sf/browser-env';
+import { socialNetworks } from '@@sf/server-data';
+import SocialNetworkIcon from '@@sf/components/SocialNetworkIcon.vue';
+
+export interface Props {
+  network: NetworkName;
+  href?: string;
+  message?: string;
+}
+
+const props = defineProps<Props>();
+const fixedHref = computed(() => {
+  let href = props.href || socialNetworks[props.network];
+  if (props.network === 'whatsapp') {
+    const tel = href.replace(/[^+\d]/g, '');
+    // eslint-disable-next-line prefer-template
+    href = `https://${(isMobile ? 'api' : 'web')}.whatsapp.com/send?phone=`
+      + encodeURIComponent(tel.charAt(0) === '+' ? tel : `+55${tel}`);
+    if (props.message) {
+      href += `&text=${encodeURIComponent(props.message)}`;
+    }
+  }
+  return href;
+});
+</script>
+
+<template>
+  <ALink :href="fixedHref">
+    <slot>
+      <SocialNetworkIcon :network="network" />
+      <slot name="append"></slot>
+    </slot>
+  </ALink>
+</template>
