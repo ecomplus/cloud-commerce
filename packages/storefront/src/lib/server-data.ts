40ac1db1a30ceb818a019ef21606c7541ceb229c
Leonardo Matos <leomp120894@gmail.com>
2023-02-18 21:27:02 -0300

    feat(storefront): Add `@@sf/server-data` exporting `settings` and `apiContext` SSRred objects
    Asbtracting `socialNetworks` object handling links from settings

---
+++ packages/storefront/src/lib/server-data.ts
@@ -0,0 +1,25 @@
+const {
+  settings,
+  context: apiContext,
+} = globalThis.storefront;
+
+const networkNames = [
+  'whatsapp',
+  'instagram',
+  'facebook',
+  'twitter',
+  'youtube',
+  'tiktok',
+  'pinterest',
+] as const;
+type NetworkName = typeof networkNames[number];
+const socialNetworks: Partial<Record<NetworkName, string>> = {};
+networkNames.forEach((network: NetworkName) => {
+  if (settings[network]) {
+    socialNetworks[network] = settings[network];
+  }
+});
+
+export { settings, apiContext, socialNetworks };
+
+export type { NetworkName };
