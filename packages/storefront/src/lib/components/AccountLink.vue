5f07602231664a2b116684da33c615c50f436585
Leonardo Matos <leomp120894@gmail.com>
2023-02-26 17:45:44 -0300

    feat(storefront): Add new `AccountLink` atomic component
    Handling default account pages URLs and arguments

---
+++ packages/storefront/src/lib/components/AccountLink.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+export interface Props {
+  to?: 'orders' | 'favorites';
+  accountUrl?: string;
+  returnUrl?: string;
+  isSignUp?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  accountUrl: globalThis.storefront.settings.account_url || '/app/account',
+  returnUrl: globalThis.location?.href,
+});
+const href = computed(() => {
+  let url = props.accountUrl;
+  if (!props.to) {
+    if (props.isSignUp) {
+      url += '?sign_up&';
+    } else {
+      url += '?';
+    }
+    return `${url}return_url=${props.returnUrl}`;
+  }
+  const { settings } = globalThis.storefront;
+  if (props.to === 'orders' && settings.orders_url) {
+    return settings.orders_url;
+  }
+  if (props.to === 'favorites' && settings.favorites_url) {
+    return settings.favorites_url;
+  }
+  return `${url}/${props.to}`;
+});
+</script>
+
+<template>
+  <a :href="href"><slot /></a>
+</template>
