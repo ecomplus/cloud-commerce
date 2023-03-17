c77b91062d0ef10ac94a355f458b2f150e7d1958
Leonardo Matos <leomp120894@gmail.com>
2023-03-17 14:35:05 -0300

    feat(storefront): New `Picture.runtime.astro` dealing with pre-compiled images :tada:
    Reading CSV files output from https://github.com/ecomplus/cloud-commerce/commit/d34e34ea16890ff3b9e8766d2ac7ecaf544927e1
    
    Fixes https://github.com/ecomplus/cloud-commerce/issues/129

---
+++ packages/storefront/src/serverless/get-image.ts
@@ -0,0 +1,47 @@
+import type { OutputFormat } from '@astrojs/image/dist/loaders/';
+import { join as joinPath } from 'node:path';
+import { readFileSync } from 'node:fs';
+
+type BuiltImage = { filename: string, width: number, height: number };
+const builtImages: BuiltImage[] = [];
+const manifestFilepath = joinPath(process.cwd(), 'dist/server/images.dist.csv');
+readFileSync(manifestFilepath, 'utf-8').split(/\n/).forEach((line) => {
+  const [filename, width, height] = line.split(',');
+  builtImages.push({
+    filename,
+    width: Number(width),
+    height: Number(height),
+  });
+});
+builtImages.sort((a, b) => {
+  if (a.width < b.width) return -1;
+  return 1;
+});
+
+type ImageOptions = Record<string, any> & {
+  src: string,
+  width: number,
+  format: OutputFormat,
+};
+const getImage = async ({ src, width, format }: ImageOptions) => {
+  const filename = src.replace(/^.*\//, '').replace(/.\w+(\?.*)?$/, '');
+  const matchFilename = (_builtImage: BuiltImage) => {
+    return filename === _builtImage.filename
+      .replace(new RegExp(`[_.][a-z0-9]+\\.${format}$`, 'i'), '');
+  };
+  let builtImage = builtImages.find((_builtImage) => {
+    return _builtImage.width >= width && matchFilename(_builtImage);
+  });
+  if (!builtImage) {
+    builtImage = builtImages.find(matchFilename);
+  }
+  return {
+    src: `/_astro/${builtImage.filename}`,
+    width: builtImage.width,
+    height: builtImage.height,
+  };
+};
+
+export default getImage;
+
+export { getImage };
