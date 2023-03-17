9cd4a90ec814388f4632f052829ddfb2d71581b5
Leonardo Matos <leomp120894@gmail.com>
2023-03-17 20:02:14 -0300

    build(storefront): Must move also png, jpg and jpeg built images [#129]

--- packages/storefront/scripts/build-prod.sh
+++ packages/storefront/scripts/build-prod.sh
@@ -3,17 +3,17 @@
 BUILD_OUTPUT=static BUILD_OUT_DIR=./.cloudcommerce/sf-tmp-dist npx astro build
 DEPLOY_RUNTIME=serverless npx astro build
 
-mv ./.cloudcommerce/sf-tmp-dist/_astro/*.{webp,avif} ./dist/client/_astro/
+mv ./.cloudcommerce/sf-tmp-dist/_astro/*.{png,jpg,jpeg,webp,avif,svg} ./dist/client/_astro/
 rm -rf ./.cloudcommerce/sf-tmp-dist
 
 identify -format "%f,%w,%h\n" \
-  ./dist/client/_astro/*.{webp,avif} > ./dist/server/images.dist.csv
+  ./dist/client/_astro/*.{png,jpg,jpeg,webp,avif,svg} > ./dist/server/images.dist.csv
 
 identify -format "assets/%f,%w,%h\n" \
-  ./public/assets/*.{png,jpg,jpeg,gif,ico,webp,avif} > ./dist/server/images.src.csv 2>/dev/null
+  ./public/assets/*.{png,jpg,jpeg,webp,avif,svg} > ./dist/server/images.src.csv 2>/dev/null
 identify -format "img/%f,%w,%h\n" \
-  ./public/img/*.{png,jpg,jpeg,gif,ico,webp,avif} >> ./dist/server/images.src.csv 2>/dev/null
+  ./public/img/*.{png,jpg,jpeg,webp,avif,svg} >> ./dist/server/images.src.csv 2>/dev/null
 identify -format "img/uploads/%f,%w,%h\n" \
-  ./public/img/uploads/*.{png,jpg,jpeg,gif,ico,webp,avif} >> ./dist/server/images.src.csv 2>/dev/null
+  ./public/img/uploads/*.{png,jpg,jpeg,webp,avif,svg} >> ./dist/server/images.src.csv 2>/dev/null
 
 exit 0
