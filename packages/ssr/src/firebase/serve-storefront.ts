3fb19b06452082f270e9e3c853e53efe1eaa4b51
Leonardo Matos <leomp120894@gmail.com>
2023-03-18 00:15:16 -0300

    feat(ssr): Simple fallback to /_image route reading images manifest
    Simulating `@astrojs/image` on server

--- packages/ssr/src/firebase/serve-storefront.ts
+++ packages/ssr/src/firebase/serve-storefront.ts
@@ -6,6 +6,9 @@ import logger from 'firebase-functions/logger';
 const { STOREFRONT_BASE_DIR } = process.env;
 const baseDir = STOREFRONT_BASE_DIR || process.cwd();
 const clientRoot = new URL(joinPath(baseDir, 'dist/client/'), import.meta.url);
+let imagesManifest: string;
+type BuiltImage = { filename: string, width: number, height: number };
+const builtImages: BuiltImage[] = [];
 
 export default (req: Request, res: Response) => {
   res.set('X-XSS-Protection', '1; mode=block');
@@ -34,6 +37,47 @@ export default (req: Request, res: Response) => {
         .send(err.toString());
     }
   };
+
+  if (req.path === '/_image') {
+    const { href } = req.query;
+    if (typeof href === 'string' && href.length > 3) {
+      const width = Number(req.query.w);
+      const format = String(req.query.f);
+      if (width > 0 && /^(webp|avif|png|jpg|jpeg)$/.test(format)) {
+        (async () => {
+          if (!imagesManifest) {
+            const manifestFilepath = joinPath(baseDir, 'dist/server/images.dist.csv');
+            imagesManifest = await readFile(manifestFilepath, 'utf-8');
+            imagesManifest.split(/\n/).forEach((line) => {
+              const [filename, _width, height] = line.split(',');
+              builtImages.push({
+                filename,
+                width: Number(_width),
+                height: Number(height),
+              });
+            });
+            builtImages.sort((a, b) => {
+              if (a.width < b.width) return -1;
+              return 1;
+            });
+          }
+          const filename = href.replace(/^.*\//, '').replace(/.\w+(\?.*)?$/, '');
+          const filenameRegExp = new RegExp(`[_.][a-z0-9]+\\.${format}$`, 'i');
+          const builtImage = builtImages.find((_builtImage) => {
+            return _builtImage.width >= width
+              && filename === _builtImage.filename.replace(filenameRegExp, '');
+          });
+          if (builtImage) {
+            return res.redirect(301, `/_astro/${builtImage.filename}`);
+          }
+          return res.sendStatus(404);
+        })();
+        return;
+      }
+    }
+    res.sendStatus(400);
+    return;
+  }
 
   /*
   https://github.com/withastro/astro/blob/main/examples/ssr/server/server.mjs
