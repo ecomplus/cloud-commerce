119cea1085fc017b6e8614fa617018e25a1cc53c
Leonardo Matos <leomp120894@gmail.com>
2023-03-17 23:45:22 -0300

    build(storefront): Prevent unecessary warnings on production build with unmatched extensions

--- packages/storefront/scripts/build-prod.sh
+++ packages/storefront/scripts/build-prod.sh
@@ -3,11 +3,11 @@
 BUILD_OUTPUT=static BUILD_OUT_DIR=./.cloudcommerce/sf-tmp-dist npx astro build
 DEPLOY_RUNTIME=serverless npx astro build
 
-mv ./.cloudcommerce/sf-tmp-dist/_astro/*.{png,jpg,jpeg,webp,avif,svg} ./dist/client/_astro/
+mv ./.cloudcommerce/sf-tmp-dist/_astro/*.{png,jpg,jpeg,webp,avif,svg} ./dist/client/_astro/ 2>/dev/null
 rm -rf ./.cloudcommerce/sf-tmp-dist
 
 identify -format "%f,%w,%h\n" \
-  ./dist/client/_astro/*.{png,jpg,jpeg,webp,avif,svg} > ./dist/server/images.dist.csv
+  ./dist/client/_astro/*.{png,jpg,jpeg,webp,avif,svg} > ./dist/server/images.dist.csv 2>/dev/null
 
 identify -format "assets/%f,%w,%h\n" \
   ./public/assets/*.{png,jpg,jpeg,webp,avif,svg} > ./dist/server/images.src.csv 2>/dev/null
