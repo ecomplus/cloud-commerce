a6aea7eeca6708a8299aa7583d43b5e6bb86712e
Leonardo Matos <leomp120894@gmail.com>
2023-04-19 21:21:53 -0300

    revert(config): Remove fallback (dev only) Store ID on config
    It should be done on storefront config point instead

--- packages/config/src/config.ts
+++ packages/config/src/config.ts
@@ -5,7 +5,6 @@ import {
   DEFAULT_CURRENCY,
   DEFAULT_CURRENCY_SYMBOL,
   DEFAULT_COUNTRY_CODE,
-  DEV_FALLBACK_STORE_ID,
 } from './defaults';
 
 type BaseConfig = {
@@ -32,11 +31,10 @@ _env.ECOM_LANG = _env.ECOM_LANG || DEFAULT_LANG;
 _env.ECOM_CURRENCY = _env.ECOM_CURRENCY || DEFAULT_CURRENCY;
 _env.ECOM_CURRENCY_SYMBOL = _env.ECOM_CURRENCY_SYMBOL || DEFAULT_CURRENCY_SYMBOL;
 _env.ECOM_COUNTRY_CODE = _env.ECOM_COUNTRY_CODE || DEFAULT_COUNTRY_CODE;
-let isNoStoreWarned = false;
 
 export default {
   get(): BaseConfig {
-    const config = {
+    return {
       lang: _env.ECOM_LANG,
       currency: _env.ECOM_CURRENCY,
       currencySymbol: _env.ECOM_CURRENCY_SYMBOL,
@@ -44,16 +42,6 @@ export default {
       storeId: Number(_env.ECOM_STORE_ID),
       ...self.config,
     };
-    if (!config.storeId && _env.DEV === true) {
-      config.storeId = DEV_FALLBACK_STORE_ID;
-      if (!isNoStoreWarned) {
-        // eslint-disable-next-line no-console
-        console.warn('[@cloudcommerce/config] `storeId` is not set, '
-          + `using dev-only fallback ${DEV_FALLBACK_STORE_ID}`);
-        isNoStoreWarned = true;
-      }
-    }
-    return config;
   },
   set(config: Partial<BaseConfig>) {
     const { cmsSettings } = config;
--- packages/config/src/defaults.ts
+++ packages/config/src/defaults.ts
@@ -5,5 +5,3 @@ export const DEFAULT_CURRENCY = 'BRL';
 export const DEFAULT_CURRENCY_SYMBOL = 'R$';
 
 export const DEFAULT_COUNTRY_CODE = 'BR';
-
-export const DEV_FALLBACK_STORE_ID = 1011;
