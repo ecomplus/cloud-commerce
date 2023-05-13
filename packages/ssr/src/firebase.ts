6655183c109b7433548c36294ae988247409671b
Leonardo Matos <leomp120894@gmail.com>
2023-05-13 20:19:06 -0300

    refactor(ssr): Updating default config memory for Functions v2
    Moving feeds away from SSR codebase (SSR may have many deploys)
    
    Functions v2 faster now with cold start boost https://twitter.com/inlined/status/1657030887014318081

--- packages/ssr/src/firebase.ts
+++ packages/ssr/src/firebase.ts
@@ -3,25 +3,13 @@ import { initializeApp } from 'firebase-admin/app';
 
 initializeApp();
 
-import functions from 'firebase-functions/v1';
 import { onRequest } from 'firebase-functions/v2/https';
 import config from '@cloudcommerce/firebase/lib/config';
 import serveStorefront from './firebase/serve-storefront';
 
-const { httpsFunctionOptions, ssrFunctionOptions } = config.get();
-const { region } = httpsFunctionOptions;
+const { ssrFunctionOptions } = config.get();
 
 export const ssr = onRequest({
   concurrency: 80,
   ...ssrFunctionOptions,
-  memory: '1GiB',
 }, serveStorefront);
-
-export const feeds = functions
-  .region(region)
-  .runWith({
-    ...httpsFunctionOptions,
-    memory: '512MB',
-    timeoutSeconds: 120,
-  })
-  .https.onRequest(serveStorefront);
--- packages/ssr/src/firebase/serve-feeds.ts
+++
@@ -1,9 +0,0 @@
-import type { Request, Response } from 'firebase-functions';
-
-export default (req: Request, res: Response) => {
-  const { method } = req;
-  if (method !== 'GET') {
-    return res.sendStatus(405);
-  }
-  return res.sendStatus(201);
-};
