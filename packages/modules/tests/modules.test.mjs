de94a42146d3ee5c16d32b3f3c6771f17ef2b0bc
Leonardo Matos <leomp120894@gmail.com>
2023-05-17 19:47:07 -0300

    chore: Add `test:e2e` command to package and turbo

--- packages/modules/tests/modules.test.mjs
+++ packages/modules/tests/modules.test.mjs
@@ -4,23 +4,23 @@ import test, { describe } from 'node:test';
 const baseUrl = 'http://127.0.0.1:5001/ecom2-002/southamerica-east1/modules';
 
 describe('Test Schemas', async () => {
-  test('@checkout', async (t) => {
+  test('@checkout', async (/* t */) => {
     const req = await fetch(`${baseUrl}/@checkout/schema`);
     assert.strictEqual(req.status, 200);
   });
-  test('apply_discount', async (t) => {
+  test('apply_discount', async () => {
     const req = await fetch(`${baseUrl}/apply_discount/schema`);
     assert.strictEqual(req.status, 200);
   });
-  test('calculate_shipping', async (t) => {
+  test('calculate_shipping', async () => {
     const req = await fetch(`${baseUrl}/calculate_shipping/schema`);
     assert.strictEqual(req.status, 200);
   });
-  test('create_transaction', async (t) => {
+  test('create_transaction', async () => {
     const req = await fetch(`${baseUrl}/create_transaction/schema`);
     assert.strictEqual(req.status, 200);
   });
-  test('list_payments', async (t) => {
+  test('list_payments', async () => {
     const req = await fetch(`${baseUrl}/list_payments/schema`);
     assert.strictEqual(req.status, 200);
   });
