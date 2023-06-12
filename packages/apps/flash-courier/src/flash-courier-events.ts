11628d66dfac67a970e75774dd8eb8249a185c7b
Leonardo Matos <leomp120894@gmail.com>
2023-06-12 20:17:23 -0300

    fix(flash-courier): Fix functions namespace and package exports filename

--- packages/apps/flash-courier/src/flash-courier-events.ts
+++ packages/apps/flash-courier/src/flash-courier-events.ts
@@ -4,7 +4,7 @@ import * as functions from 'firebase-functions/v1';
 import config from '@cloudcommerce/firebase/lib/config';
 import handleTrackingCodes from '../lib-mjs/update-tracking.mjs';
 
-export const melhorenvio = {
+export const flashcourier = {
   cronTrackingCodes: functions.region(config.get().httpsFunctionOptions.region).pubsub
     .schedule('19 * * * *')
     .onRun(() => {
