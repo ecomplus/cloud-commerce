0965ab7bad303e75cda0c1dd7467616bcce67bc5
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-06-13 17:23:08 -0300

    fix(affilate program): Fix app event error

--- packages/apps/affilate-program/src/events-to-affilate-program.ts
+++ packages/apps/affilate-program/src/events-to-affilate-program.ts
@@ -1,5 +1,10 @@
 import type { ApiEventHandler } from '@cloudcommerce/firebase/lib/helpers/pubsub';
-import type { ResourceId, Customers, Orders } from '@cloudcommerce/api/types';
+import type {
+  ResourceId,
+  Customers,
+  Orders,
+  ResourceAndId,
+} from '@cloudcommerce/api/types';
 import { getFirestore, Timestamp } from 'firebase-admin/firestore';
 import { logger } from 'firebase-functions/v1';
 import api from '@cloudcommerce/api';
@@ -29,7 +34,7 @@ const handleApiEvent: ApiEventHandler = async ({
   }
 
   let newCustomerId: ResourceId | undefined;
-  let newCustomerEmail: string | undefined;
+  let newCustomerEmail: Customers['main_email'] | undefined;
   let referralId: ResourceId | undefined;
   if (isCustomer) {
     const customer = apiDoc as Customers;
@@ -44,24 +49,22 @@ const handleApiEvent: ApiEventHandler = async ({
     }
   }
 
-  let orderId: string | undefined;
+  let orderId: Orders['_id'] | undefined;
   let buyerId: ResourceId | undefined;
-  let orderSubtotal: number | undefined;
-  let orderStatus: string | undefined;
-  let isPaid: boolean | undefined;
-  let isCancelled: boolean | undefined;
+  let orderSubtotal: number = 0;
+  let isPaid: boolean = false;
+  let isCancelled: boolean = false;
 
   if (isOrder) {
     const order = apiDoc as Orders;
     orderId = order._id;
-    orderSubtotal = order.amount?.subtotal;
-    orderStatus = order.financial_status?.current;
+    orderSubtotal = order.amount?.subtotal || 0;
+    const orderStatus = order.financial_status?.current;
     buyerId = order.buyers?.[0]?._id;
     if (!orderId || !orderSubtotal || !orderStatus || !buyerId) {
       return null;
     }
-    isPaid = false;
-    isCancelled = false;
+
     switch (orderStatus) {
       case 'paid':
         isPaid = true;
@@ -78,99 +81,104 @@ const handleApiEvent: ApiEventHandler = async ({
     if (!isPaid && !isCancelled) {
       return null;
     }
-
-    logger.info(`${isOrder ? 'Orders' : 'Customers'}`);
-    try {
-      if (isCustomer && appData.points_on_signup) {
-        const referralCustomer = (await api.get(`customers/${referralId}`)).data;
-        if (!referralCustomer?.enabled) {
-          return null;
-        }
-        if (referralCustomer.main_email === newCustomerEmail) {
-          return null;
-        }
-        // save new customer points
-        const pointsEndpoint = `customers/${newCustomerId}/loyalty_points_entries` as `customers/${ResourceId}/string`;
-        const d = new Date();
-        d.setDate(d.getDate() + 120);
-        const validThru = d.toISOString();
-        await api.post(
-          pointsEndpoint,
-          {
-            name: 'Convite',
-            program_id: 'referreds0',
-            earned_points: appData.points_on_signup,
-            active_points: appData.points_on_signup,
-            ratio: 1,
-            valid_thru: validThru,
-          },
-        );
-      }
-
-      if (isOrder && isPaid && appData.points_on_referral) {
-        if (appData.min_subtotal_to_earn > orderSubtotal) {
-          return null;
-        }
-        const buyerCustomer = (await api.get(`customers/${buyerId}`)).data;
-
-        if (!buyerCustomer?.enabled) {
-          return null;
-        }
-
-        if (!buyerCustomer.affiliate_code) {
-          return null;
-        }
-        const referralCustomer = (await api.get(`customers/${buyerCustomer.affiliate_code}`)).data;
-        if (referralCustomer.doc_number === buyerCustomer.doc_number) {
-          return null;
-        }
-        logger.info(`Order ${orderId} paid by ${buyerId} validated`);
-
-        const docRef = firestoreColl.doc(`${orderId}`);
-        if (!appData.on_all_orders) {
-          // check if points not already paid for this order
-          const docSnapshot = await docRef.get();
-          if (docSnapshot.exists) {
-            return null;
-          }
-        }
-        let earnedPoints: number;
-        if (typeof appData.points_on_referral === 'number') {
-          earnedPoints = appData.points_on_referral;
-        } else if (appData.points_on_referral.type === 'percentage') {
-          earnedPoints = (orderSubtotal * appData.points_on_referral.value) / 100;
-        } else {
-          earnedPoints = appData.points_on_referral.value;
-        }
-        if (!(earnedPoints > 0)) {
-          return null;
-        }
-        const pointsEndpoint = `/customers/${buyerCustomer.affiliate_code}/loyalty_points_entries`;
-        await api.post(
-          pointsEndpoint,
-          {
-            name: 'Afiliado',
-            program_id: 'affiliates0',
-            earned_points: earnedPoints,
-            active_points: earnedPoints,
-            ratio: 1,
-          },
-        );
-        await docRef.set({
-          buyerId,
-          referralId: buyerCustomer.affiliate_code,
-          earnedPoints,
-          t: Timestamp.fromDate(new Date()),
-        });
-      }
-
-      return null;
-    } catch (err) {
-      logger.error(err);
-      return null;
-    }
   }
-  return null;
+
+  logger.info(`${isOrder ? 'Orders' : 'Customers'}`);
+  try {
+    if (isCustomer && appData.points_on_signup) {
+      const referralCustomer = (await api.get(`customers/${referralId}`)).data;
+      if (!referralCustomer?.enabled) {
+        logger.warn('Inactive customer');
+        return null;
+      }
+      if (referralCustomer.main_email === newCustomerEmail) {
+        logger.warn('new customer is the affiliate, same email');
+        return null;
+      }
+      // save new customer points
+      // TODO: set type for partial body in post method
+      const pointsEndpoint = `customers/${newCustomerId}/loyalty_points_entries` as `${ResourceAndId}/string`;
+      const d = new Date();
+      d.setDate(d.getDate() + 120);
+      const validThru = d.toISOString();
+      await api.post(
+        pointsEndpoint,
+        {
+          name: 'Convite',
+          program_id: 'referreds0',
+          earned_points: appData.points_on_signup,
+          active_points: appData.points_on_signup,
+          ratio: 1,
+          valid_thru: validThru,
+        },
+      );
+    }
+
+    if (isOrder && isPaid && appData.points_on_referral && orderId) {
+      if (appData.min_subtotal_to_earn > orderSubtotal) {
+        return null;
+      }
+      const buyerCustomer = (await api.get(`customers/${buyerId}`)).data;
+
+      if (!buyerCustomer?.enabled) {
+        logger.warn('Inactive customer');
+        return null;
+      }
+
+      if (!buyerCustomer.affiliate_code) {
+        return null;
+      }
+      const referralCustomer = (await api.get(`customers/${buyerCustomer.affiliate_code}`)).data;
+      if (referralCustomer.doc_number === buyerCustomer.doc_number) {
+        logger.warn('Order placed by affiliate');
+        return null;
+      }
+      logger.info(`Order ${orderId} paid by ${buyerId} validated`);
+
+      const docRef = firestoreColl.doc(`${orderId}`);
+      if (!appData.on_all_orders) {
+        // check if points not already paid for this order
+        const docSnapshot = await docRef.get();
+        if (docSnapshot.exists) {
+          return null;
+        }
+      }
+      let earnedPoints: number;
+      if (typeof appData.points_on_referral === 'number') {
+        earnedPoints = appData.points_on_referral;
+      } else if (appData.points_on_referral.type === 'percentage') {
+        earnedPoints = (orderSubtotal * appData.points_on_referral.value) / 100;
+      } else {
+        earnedPoints = appData.points_on_referral.value;
+      }
+      if (!(earnedPoints > 0)) {
+        return null;
+      }
+      // TODO: set type for partial body in post method
+      const pointsEndpoint = `/customers/${buyerCustomer.affiliate_code}/loyalty_points_entries` as `${ResourceAndId}/string`;
+      await api.post(
+        pointsEndpoint,
+        {
+          name: 'Afiliado',
+          program_id: 'affiliates0',
+          earned_points: earnedPoints,
+          active_points: earnedPoints,
+          ratio: 1,
+        },
+      );
+      await docRef.set({
+        buyerId,
+        referralId: buyerCustomer.affiliate_code,
+        earnedPoints,
+        t: Timestamp.fromDate(new Date()),
+      });
+    }
+
+    return null;
+  } catch (err) {
+    logger.error(err);
+    return null;
+  }
 };
 
 export default handleApiEvent;
