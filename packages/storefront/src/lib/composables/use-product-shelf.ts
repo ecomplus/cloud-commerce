650fc6e0179330d3cc0e4a19ddf1d7e63f0a8c91
Leonardo Matos <leomp120894@gmail.com>
2023-06-19 20:28:02 -0300

    fix(storefront): Properly handling product shelf options
    Add parsers on `useSections` Astro composable to handle product shelf CMS "shortcuts" and compatibilty

--- packages/storefront/src/lib/composables/use-product-shelf.ts
+++ packages/storefront/src/lib/composables/use-product-shelf.ts
@@ -1,12 +1,14 @@
-import type { SearchItems } from '@cloudcommerce/types';
+import type { ResourceId, Collections, SearchItems } from '@cloudcommerce/types';
 import { ref, computed } from 'vue';
 import api from '@cloudcommerce/api';
+import { inStock as checkInStock } from '@ecomplus/utils';
 
 export interface Props {
-  collectionId: string | null;
-  sort?: 'sales' | 'news' | 'lowest_price' | 'highest_price'
-    | 'offers' | 'price_discount' | 'views';
+  collectionId: ResourceId | null;
+  searchQuery?: `&${string}` | '';
+  sort?: '-sales' | '-created_at' | 'price' | '-price' | '-price_discount' | string;
   title?: string | null;
+  titleLink?: string | null;
   isShuffle?: boolean;
   limit?: number;
   page?: number;
@@ -14,29 +16,73 @@ export interface Props {
 }
 
 const useProductShelf = (props: Props) => {
+  const title = ref<string | null>(props.title || '');
+  const titleLink = ref<string | null>(props.titleLink || '');
   const isFetching = ref(false);
-  const fetching = ref<Promise<void> | null>(null);
+  const fetching = ref<Promise<any> | null>(null);
   const fetchError = ref<Error | null>(null);
   const _products = ref<SearchItems>([]);
   const products = computed(() => {
     if (props.products) return props.products;
     return _products.value;
   });
+
   if (!props.products) {
     isFetching.value = true;
-    fetching.value = api.get('search/v1')
-      .then(({ data }) => {
+    fetching.value = (async () => {
+      let searchQuery = props.searchQuery || '';
+      let collection: Collections | undefined;
+      if (props.collectionId) {
+        try {
+          const { data } = await api.get(`collections/${props.collectionId}`);
+          collection = data;
+        } catch (err: any) {
+          console.error(err);
+          fetchError.value = err;
+        }
+        const productIds = collection?.products;
+        if (Array.isArray(productIds) && productIds.length) {
+          searchQuery += `&_id=${productIds.slice(0, 60).join(',')}`;
+        }
+        if (!title.value && title.value !== null && collection?.name) {
+          title.value = collection?.name;
+        }
+      }
+      const limit = props.limit || 24;
+      const offset = props.page ? (props.page - 1) * limit : 0;
+      let endpointQuery = `offset=${offset}&limit=${limit}`;
+      if (props.sort) {
+        endpointQuery += `&sort=${props.sort}`;
+      }
+      endpointQuery += searchQuery;
+      try {
+        const { data } = await api.get(`search/v1?${endpointQuery}`);
+        if (props.isShuffle) {
+          let m = data.result.filter((item) => {
+            return item.available && checkInStock(item);
+          }).length;
+          let t: typeof data.result[0];
+          let i: number;
+          while (m) {
+            // eslint-disable-next-line no-plusplus
+            i = Math.floor(Math.random() * m--);
+            t = data.result[m];
+            data.result[m] = data.result[i];
+            data.result[i] = t;
+          }
+        }
         _products.value = data.result;
-      })
-      .catch((err) => {
+      } catch (err: any) {
         console.error(err);
         fetchError.value = err;
-      })
-      .finally(() => {
-        isFetching.value = false;
-      });
+      }
+      isFetching.value = false;
+    })();
   }
+
   return {
+    title,
+    titleLink,
     isFetching,
     fetching,
     fetchError,
--- packages/storefront/src/lib/layouts/sections/use-sections.ts
+++ packages/storefront/src/lib/layouts/sections/use-sections.ts
@@ -1,3 +1,4 @@
+import type { ResourceId } from '@cloudcommerce/types';
 import type { RouteContext } from '@@sf/ssr-context';
 import type { Props as UseProductShelf } from '@@sf/composables/use-product-shelf';
 
@@ -16,20 +17,58 @@ const useSections = async ({ routeContext }: Props) => {
     sectionsContent.forEach(({ type, ...sectionContent }) => {
       if (type === 'product-shelf') {
         const {
-          collection_id: collectionId,
-          sort,
-          title,
+          collection_id: collectionIdAndInfo,
           headless: isHeadless,
           shuffle: isShuffle,
           ...rest
         } = sectionContent;
+        let { sort, title } = sectionContent;
+        switch (sort) {
+          case 'offers':
+            sort = '-price_discount';
+            break;
+          case 'news':
+            sort = '-_id';
+            break;
+          case 'lowest_price':
+            sort = 'price';
+            break;
+          case 'highest_price':
+            sort = '-price';
+            break;
+          default:
+        }
+        let collectionId: ResourceId | null = null;
+        let searchQuery: `&${string}` | undefined;
+        let titleLink: string | undefined;
+        if (collectionIdAndInfo) {
+          const [_id, resource, name, path] = (collectionIdAndInfo as string)
+            .split(':') as [
+              ResourceId,
+              string | undefined,
+              string | undefined,
+              string | undefined,
+            ];
+          collectionId = _id;
+          if (resource === 'categories') {
+            searchQuery = `&categories._id=${_id}`;
+          } else if (resource === 'brands') {
+            searchQuery = `&brands._id=${_id}`;
+          }
+          if (!title && title !== null && name) {
+            title = name;
+          }
+          titleLink = path;
+        }
         sections.push({
           type,
           props: {
             ...rest,
             collectionId,
-            sort: sort as 'views',
+            searchQuery,
+            sort,
             title: isHeadless ? null : title,
+            titleLink,
             isShuffle,
           },
         });
