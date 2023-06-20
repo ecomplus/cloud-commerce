782fbce886bbab186bc6ff750a4f2b778fe9a867
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-06-20 13:24:32 -0300

    chore(test base): Get request payloads

--- packages/test-base/README.md
+++ packages/test-base/README.md
@@ -1,1 +1,7 @@
 # `@cloudcommerce/test-base`
+
+It is necessary to add the payloads folder to the directory containing the following files:
+
+1) calculate-shipment.json
+2) create-transaction.json
+3) list-payments.json
--- packages/test-base/src/playloads.ts
+++ packages/test-base/src/playloads.ts
@@ -1,28 +1,24 @@
-const bodyCalculateShipping = {
-  to: { zip: '35701000' },
-  items: [{
-    product_id: '6166cb1528ace502aea2dc36',
-    sku: 'GIE2742',
-    name: 'Vaso para orquídeas',
-    quantity: 1,
-    currency_id: 'BRL',
-    currency_symbol: 'R$',
-    price: 24.99,
-    dimensions: {
-      width: { unit: 'cm', value: 30 },
-      height: { unit: 'cm', value: 30 },
-      length: { unit: 'cm', value: 30 },
-    },
-    weight: { unit: 'kg', value: 0.5 },
-  }],
-  subtotal: 24.99,
+import url from 'node:url';
+import path from 'node:path';
+import fs from 'node:fs';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+const getPayload = (pathFile: string) => {
+  return JSON.parse(
+    fs.readFileSync(
+      path.join(__dirname, pathFile),
+      { encoding: 'utf-8' },
+    ),
+  );
 };
 
-const bodyListPayments = {
-
-};
+const bodyCalculateShipping = getPayload('../payloads/calculate-shipping.json');
+const bodyListPayments = getPayload('../payloads/list-payments.json');
+const bodyCreateTransaction = getPayload('../payloads/create-transaction.json');
 
 export {
   bodyCalculateShipping,
   bodyListPayments,
+  bodyCreateTransaction,
 };
