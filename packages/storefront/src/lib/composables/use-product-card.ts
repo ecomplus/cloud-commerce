bf19fa33730eb18be95d56d54424a8ceb521938c
Leonardo Matos <leomp120894@gmail.com>
2023-06-20 22:36:29 -0300

    feat(storefront): Additional computed within `useProductCard`
    From current `@ecomplus/storefront-components` <ProductCard> implementation

--- packages/storefront/src/lib/composables/use-product-card.ts
+++ packages/storefront/src/lib/composables/use-product-card.ts
@@ -1,12 +1,19 @@
-import type { ResourceId, Products, SearchItem } from '@cloudcommerce/types';
+import type { Products, Carts, SearchItem } from '@cloudcommerce/types';
 import { ref, computed, shallowReactive } from 'vue';
 import api from '@cloudcommerce/api';
-import { name as getName } from '@ecomplus/utils';
+import {
+  name as getName,
+  price as getPrice,
+  inStock as checkInStock,
+  onPromotion as checkOnPromotion,
+} from '@ecomplus/utils';
+
+type ProductItem = Products | SearchItem | Carts['items'][0];
 
 export type Props = {
-  product?: Products | SearchItem;
-  productId?: ResourceId;
-} & ({ product: Products | SearchItem } | { productId: ResourceId });
+  product?: ProductItem;
+  productId?: Products['_id'];
+} & ({ product: ProductItem } | { productId: Products['_id'] });
 
 const useProductCard = (props: Props) => {
   const isFetching = ref(false);
@@ -15,6 +22,23 @@ const useProductCard = (props: Props) => {
   const product = shallowReactive<Products | SearchItem | {}>(props.product || {});
   const title = computed(() => {
     return getName(product);
+  });
+  const price = computed(() => {
+    return getPrice(product);
+  });
+  const discount = computed(() => {
+    if (checkOnPromotion(product)) {
+      const basePrice = (product as Products).base_price as number;
+      return Math.round(((basePrice - price.value) * 100) / basePrice);
+    }
+    return 0;
+  });
+  const isInStock = computed(() => {
+    return checkInStock(product);
+  });
+  const isActive = computed(() => {
+    return isInStock.value
+     && (product as Products).available && (product as Products).visible;
   });
 
   const { productId } = props;
@@ -34,6 +58,10 @@ const useProductCard = (props: Props) => {
 
   return {
     title,
+    price,
+    discount,
+    isInStock,
+    isActive,
     isFetching,
     fetching,
     fetchError,
