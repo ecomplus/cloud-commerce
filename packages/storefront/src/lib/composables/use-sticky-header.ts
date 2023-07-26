c6730b25a2f2816cea8595da627e20828cce4cdb
Leonardo Matos <leomp120894@gmail.com>
2023-07-26 12:49:08 -0300

    chore(storefront): Updating minor DOM deprecation marks

--- packages/storefront/src/lib/composables/use-sticky-header.ts
+++ packages/storefront/src/lib/composables/use-sticky-header.ts
@@ -63,7 +63,7 @@ const useStickyHeader = (props: Props) => {
         const headerElm = header.value as HTMLElement;
         staticHeight.value = headerElm.offsetHeight;
         staticY.value = staticHeight.value
-          + window.pageYOffset + headerElm.getBoundingClientRect().top;
+          + window.scrollY + headerElm.getBoundingClientRect().top;
         start();
       };
       const imgs = header.value.getElementsByTagName('IMG');
