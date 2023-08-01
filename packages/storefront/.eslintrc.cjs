b05e12c23a7cf3ffd5fee6e7d13d498b5acbd02a
Leonardo Matos <leomp120894@gmail.com>
2023-07-31 22:38:21 -0300

    chore(eslint): Add new `@cloudcommerce/eslint` package

--- packages/storefront/.eslintrc.cjs
+++ packages/storefront/.eslintrc.cjs
@@ -1,28 +1,3 @@
 module.exports = {
-  extends: './.base.eslintrc.cjs',
-  rules: {
-    'no-console': 'off',
-    'semi': 'warn',
-    'arrow-parens': 'warn',
-    'object-curly-newline': 'warn',
-    'comma-dangle': 'warn',
-    'consistent-return': 'warn',
-    'array-bracket-spacing': 'warn',
-    'quote-props': 'off',
-    'quotes': 'off',
-    'global-require': 'off',
-    'import/no-dynamic-require': 'off',
-    'import/prefer-default-export': 'warn',
-    'import/extensions': 'off',
-    'vue/multi-word-component-names': ['error', {
-      ignores: [
-        'Fade',
-        'Carousel',
-        'Drawer',
-        'Prices',
-        'Banner',
-        'Countdown',
-      ],
-    }],
-  },
+  extends: './node_modules/@cloudcommerce/eslint/storefront.eslintrc.cjs',
 };
