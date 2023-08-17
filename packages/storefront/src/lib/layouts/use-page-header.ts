e56603848af4013eb29b7604d0fb936238e85d7c
Leonardo Matos <leomp120894@gmail.com>
2023-08-17 15:06:10 -0300

    refactor(storefront): Update `usePageHeader` composable removing `serviceLinks` prop
    Service links should be get directly from settings content instead
    
    BREAKING CHANGE: `usePageHeader` composable props changed (simplified)

--- packages/storefront/src/lib/layouts/use-page-header.ts
+++ packages/storefront/src/lib/layouts/use-page-header.ts
@@ -1,12 +1,9 @@
 import type { RouteContext } from '@@sf/ssr-context';
-import type { LayoutContent } from '@@sf/content';
 import type { Props as UseShopHeaderProps } from '@@sf/composables/use-shop-header';
 import api from '@cloudcommerce/api';
 import { parseLayoutContent } from '@@sf/composables/use-pitch-bar';
 
-type ShopHeaderProps = Omit<UseShopHeaderProps, 'header'> & {
-  serviceLinks?: LayoutContent['service_links'],
-};
+type ShopHeaderProps = Omit<UseShopHeaderProps, 'header'>;
 
 export interface Props {
   routeContext: RouteContext;
@@ -16,10 +13,7 @@ export interface Props {
 const usePageHeader = async ({ routeContext, listedCategoryFields }: Props) => {
   const { apiState, getContent } = routeContext;
   const layoutContent = await getContent('layout');
-  const {
-    header: headerContent,
-    service_links: serviceLinks,
-  } = layoutContent;
+  const { header: headerContent } = layoutContent;
   const pitchBar = parseLayoutContent(layoutContent);
   let { categories } = apiState;
   if (!categories) {
@@ -45,7 +39,6 @@ const usePageHeader = async ({ routeContext, listedCategoryFields }: Props) => {
     menuCategorySlugs: headerContent.inline_menu_categories?.featured,
     menuRandomCategories: headerContent.inline_menu_categories?.random,
     isAlphabeticalSortSubmenu: headerContent.alphabetical_sort_submenu,
-    serviceLinks,
   };
   /*
   if (import.meta.env.DEV) {
