2ba386fb26dcb72a5c91f4740403302cb4e4a157
Leonardo Matos <leomp120894@gmail.com>
2023-08-17 15:08:59 -0300

    feat(storefront): New <PaymentMethodFlag> component
    Also exporting payment flags (and type) and service links from `@@sf/sf-lib` (server data)

---
+++ packages/storefront/src/lib/components/PaymentMethodFlag.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { PaymentMethodFlag } from '@@sf/sf-lib';
+import { computed } from 'vue';
+
+export interface Props {
+  as?: string;
+  flag: PaymentMethodFlag;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  as: 'i',
+});
+const iconClassName = computed(() => {
+  switch (props.flag) {
+    case 'pix': return 'i-pix';
+    case 'visa': return 'i-visa';
+    case 'mastercard': return 'i-mastercard';
+    case 'elo': return 'i-elo';
+    case 'amex': return 'i-amex';
+    case 'hipercard': return 'i-hipercard';
+    case 'diners': return 'i-dinersclub';
+    case 'discover': return 'i-discover';
+    default: return '';
+  }
+});
+</script>
+
+<template>
+  <b v-if="!iconClassName" class="tracking-tighter text-xs">
+    {{ props.flag }}
+  </b>
+  <component v-else :is="as" :class="iconClassName" />
+</template>
