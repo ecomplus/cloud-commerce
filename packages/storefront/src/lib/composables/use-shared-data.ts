851ed67037ebc1552540bf8e4f7d1797c0807c14
Leonardo Matos <leomp120894@gmail.com>
2023-08-27 19:59:04 -0300

    feat(storefront): Use event emitter on `useSharedData` to support optional awaited value
    BREAKING CHANGE: `useSharedData` composable now returns a promise

--- packages/storefront/src/lib/composables/use-shared-data.ts
+++ packages/storefront/src/lib/composables/use-shared-data.ts
@@ -1,11 +1,26 @@
+import { EventEmitter } from 'node:events';
+
 export interface Props {
   field: string;
-  value: any;
+  value?: any;
 }
 
-const useSharedData = ({ field, value }: Props) => {
-  global.$storefront.data[field] = value;
+const emitter = new EventEmitter();
+
+const useSharedData = async ({ field, value }: Props) => {
+  const $data = global.$storefront.data;
+  if (value) {
+    $data[field] = value;
+    emitter.emit(field, value);
+  } else if ($data[field]) {
+    value = $data[field];
+  } else {
+    value = await new Promise((resolve) => {
+      emitter.once(field, resolve);
+    });
+  }
   return {
+    value,
     inlineClientJS: `
 window.$storefront.data['${field}'] = ${JSON.stringify(value)};`,
   };
