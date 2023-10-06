03b1d8da4fb01f0bc8dc7701ff71ad9ce0bdd858
Leonardo Matos <leomp120894@gmail.com>
2023-10-06 16:40:28 -0300

    fix(storefront): Minor fix `<ContentClearfix>` with horizontal scroll on large devices

--- packages/storefront/src/lib/components/ContentClearfix.vue
+++ packages/storefront/src/lib/components/ContentClearfix.vue
@@ -8,7 +8,7 @@ const parsedHTML = html.replace(/<(img|iframe) /g, '<$1 loading="lazy" ');
 </script>
 
 <template>
-  <article class="prose !max-w-full overflow-x-auto lg:overflow-x-hidden" v-once>
+  <article class="prose !max-w-full overflow-x-auto" v-once>
     <div
       class="[&_iframe]:max-w-full [&_img]:max-w-full [&_table]:max-w-full"
       v-html="parsedHTML"
