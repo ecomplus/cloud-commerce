529514968bb786cb4754912837aa9505989f3873
Leonardo Matos <leomp120894@gmail.com>
2023-10-07 18:01:55 -0300

    feat(storefront): New optional props `cartId` and `cartItem` for `<CheckoutLink>`

--- packages/storefront/src/lib/components/CheckoutLink.vue
+++ packages/storefront/src/lib/components/CheckoutLink.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import type { CartItem } from '@@sf/composables/use-cart-item';
 import { computed } from 'vue';
 
 export interface Props {
   to?: 'cart' | 'checkout';
   cartUrl?: string;
   checkoutUrl?: string;
+  cartId?: CartItem['_id'];
+  cartItem?: CartItem;
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -13,8 +16,36 @@ const props = withDefaults(defineProps<Props>(), {
   checkoutUrl: globalThis.$storefront.settings.checkoutUrl || '/app/#/checkout',
 });
 const href = computed(() => {
-  if (props.to === 'cart') return props.cartUrl;
-  return props.checkoutUrl;
+  let url = (props.to === 'cart') ? props.cartUrl : props.checkoutUrl;
+  if (props.cartId) {
+    url += `/${props.cartId}`;
+  } else if (props.cartItem) {
+    const params: Record<string, string> = {
+      product_id: props.cartItem.product_id,
+    };
+    const {
+      variation_id: variationId,
+      quantity,
+      sku,
+      customizations,
+    } = props.cartItem;
+    if (variationId) params.variation_id = variationId;
+    if (quantity) params.quantity = String(quantity);
+    if (sku) params.sku = sku;
+    if (customizations) {
+      customizations.forEach(({ label, option, attachment }, i) => {
+        const prefix = `customizations.${i}.`;
+        if (label) params[prefix + 'label'] = label;
+        if (option) {
+          if (option._id) params[prefix + 'option._id'] = option._id;
+          params[prefix + 'option.text'] = option.text;
+        }
+        if (attachment) params[prefix + 'attachment'] = attachment;
+      });
+    }
+    url += '?' + new URLSearchParams(params).toString();
+  }
+  return url;
 });
 </script>
 
