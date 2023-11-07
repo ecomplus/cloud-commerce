bf82995cbedb47e9ee6eb92596bceb5f27537107
Leonardo Matos <leomp120894@gmail.com>
2023-11-07 15:39:38 -0300

    chore(pagarme-v5): Renaming (make it simpler and fix typos) some lib filenames

--- packages/apps/pagarme-v5/src/pagarme-v5.ts
+++ packages/apps/pagarme-v5/src/pagarme-v5.ts
@@ -1,7 +1,7 @@
 import '@cloudcommerce/firebase/lib/init';
 import type { AppModuleBody } from '@cloudcommerce/types';
-import handleListPayments from '../lib-mjs/pagarme-v5-list-payments.mjs';
-import handleCreateTransaction from '../lib-mjs/pagarme-v5-create-transaction.mjs';
+import handleListPayments from '../lib-mjs/list-pagarme5-payments.mjs';
+import handleCreateTransaction from '../lib-mjs/create-pagarme5-transaction.mjs';
 
 export const listPayments = async (modBody: AppModuleBody) => {
   return handleListPayments(modBody);
