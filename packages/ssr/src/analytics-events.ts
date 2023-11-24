1f98e035d478c9226e2352a0c55ebde156ca68cc
Wisley Alves <33531578+wisley7l@users.noreply.github.com>
2023-11-23 21:55:32 -0300

    feat(ssr): Start sending GA4 server events with Measurement Protocol API (#273)
    * feat(analytics ssr): Send events to analytics providers
    
    * fix(ssr analytics): Send batch event request, page view event parser
    
    * fix(ssr analytics): Update variable names
    
    * Update packages/ssr/src/analytics-providers/google-analytics.ts
    
    * Update packages/ssr/src/analytics-providers/google-analytics.ts
    
    * Update packages/ssr/src/analytics-providers/google-analytics.ts
    
    ---------
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>

--- packages/ssr/src/analytics-events.ts
+++ packages/ssr/src/analytics-events.ts
@@ -1,8 +1,11 @@
+import type { AxiosResponse } from 'axios';
 import { EventEmitter } from 'node:events';
+import ga4Events from './analytics-providers/google-analytics';
+import metaEvents from './analytics-providers/meta-conversions-api';
 
 const analyticsEmitter = new EventEmitter();
 
-type AnalyticsEvent = {
+export type AnalyticsEvent = {
   name: string,
   params?: Record<string, any>,
 }
@@ -21,16 +24,27 @@ const sendAnalyticsEvents = (
     }
     (eventsByType[type] as AnalyticsEvent[]).push({ name, params });
   });
+
+  const sendingEvents: Promise<AxiosResponse<any, any>>[] = [];
   if (eventsByType.gtag) {
-    console.log('events to ga4:', {
-      events: eventsByType.gtag,
-      page_title: payload.page_title,
-      client_id: payload.g_client_id || payload.client_id,
-      session_id: payload.g_session_id || payload.session_id,
-      gclid: payload.gclid,
-      user_agent: payload.user_agent,
-      ip: payload.ip,
-    });
+    const sessionId = payload.g_session_id || payload.session_id;
+    const clientId = payload.g_client_id || payload.client_id;
+    const listGA4Events = ga4Events(eventsByType.gtag, clientId, sessionId, payload.utm);
+    if (listGA4Events) {
+      sendingEvents.push(...listGA4Events);
+    }
+  }
+  if (eventsByType.fbq) {
+    const userData = {
+      client_ip_address: payload.id,
+      client_user_agent: payload.user_agent,
+      fbc: payload.fbclid,
+      fbp: payload.fbp,
+    };
+    const listMetaEvents = metaEvents(eventsByType.fbq, payload.page_location, userData);
+    if (listMetaEvents) {
+      sendingEvents.push(...listMetaEvents);
+    }
   }
   /* @TODO:
   - Get credentials from env vars, e.g. `process.env.GA_MEASUREMENT_ID`;
@@ -41,6 +55,7 @@ const sendAnalyticsEvents = (
   - Send the events in parallel for all APIs with `Promise.all(sendingEvents)`;
   - No CORS!
   */
+  Promise.all(sendingEvents);
 };
 
 export default sendAnalyticsEvents;
---
+++ packages/ssr/src/analytics-providers/google-analytics.ts
@@ -0,0 +1,67 @@
+import type { AnalyticsEvent } from '../analytics-events';
+import axios, { AxiosResponse } from 'axios';
+
+const ga4Axios = axios.create({
+  baseURL: 'https://www.google-analytics.com',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+const url = `/mp/collect?api_secret=${process.env.GA_API_TOKEN}`
+  + `&measurement_id=${process.env.GA_MEASUREMENT_ID}`;
+
+const sendEvent = (
+  analyticsEvents: AnalyticsEvent[],
+  clientId: string,
+  sessionId: string,
+  utm?: {
+    source?: string,
+    medium?: string,
+    campaign?: string,
+    term?: string,
+    content?: string
+  },
+) => {
+  if (process.env.GA_API_TOKEN && process.env.GA_MEASUREMENT_ID) {
+    const requests: Promise<AxiosResponse<any, any>>[] = [];
+    let events: AnalyticsEvent[] = [];
+
+    analyticsEvents.forEach((event, index) => {
+      if (event.params) {
+        Object.assign(event.params, { session_id: sessionId });
+      }
+
+      const body = event;
+      if (body.name === 'page_view') {
+        body.name = 'campaign_details';
+        body.params = {
+          source: utm?.source,
+          medium: utm?.medium,
+          campaign: utm?.campaign,
+          term: utm?.term,
+          content: utm?.content,
+          session_id: sessionId,
+        };
+      }
+
+      events.push(body);
+      if (events.length === 25 || index === analyticsEvents.length - 1) {
+        events.push(body);
+        requests.push(
+          ga4Axios.post(url, {
+            client_id: clientId,
+            events,
+          }),
+        );
+        events = [];
+      }
+    });
+
+    return requests;
+  }
+  // throw new Error('Google Analytics credentials not found');
+  return null;
+};
+
+export default sendEvent;
