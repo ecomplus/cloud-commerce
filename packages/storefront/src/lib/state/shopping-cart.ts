21e3a28ade77503579d7e6d5fbbd3f4eec2d5248
Leonardo Matos <leomp120894@gmail.com>
2023-11-26 04:01:32 -0300

    fix(storefront): Ensure `shoppingCart` items state reactivity
    New `resetCartItems` method
    
    BREAKING CHANGE: `shoppingCart` rewriten to reactive object instead of computed

--- packages/storefront/src/lib/state/shopping-cart.ts
+++ packages/storefront/src/lib/state/shopping-cart.ts
@@ -1,6 +1,6 @@
 import type { Products, CartSet, SearchItem } from '@cloudcommerce/api/types';
-import { computed } from 'vue';
-import { watchDebounced } from '@vueuse/core';
+import { computed, watch } from 'vue';
+import { useDebounceFn } from '@vueuse/core';
 import mitt from 'mitt';
 import useStorage from '@@sf/state/use-storage';
 import addItem from '@@sf/state/shopping-cart/add-cart-item';
@@ -11,10 +11,78 @@ const storageKey = 'ecomShoppingCart';
 const emptyCart = {
   items: [],
 };
-const cart = useStorage<CartSet>(storageKey, emptyCart);
+const shoppingCart = useStorage<CartSet>(storageKey, emptyCart);
+const totalItems = computed(() => {
+  return shoppingCart.items.reduce((acc, item) => {
+    return acc + item.quantity;
+  }, 0);
+});
 
-const cartItems = computed(() => {
-  return cart.items.map((item) => {
+const addCartItem = (newItem: CartItem) => {
+  addItem(shoppingCart, newItem);
+};
+const removeCartItem = (itemId: string) => {
+  for (let i = 0; i < shoppingCart.items.length; i++) {
+    const item = shoppingCart.items[i];
+    if (item._id === itemId) {
+      shoppingCart.items.splice(i, 1);
+      break;
+    }
+  }
+};
+const resetCartItems = (items?: CartItem[]) => {
+  while (shoppingCart.items.length) {
+    shoppingCart.items.pop();
+  }
+  items?.forEach(addCartItem);
+};
+const addProductToCart = (
+  product: (Partial<Products> | Partial<SearchItem>) & { _id: Products['_id'] },
+  variationId?: Products['_id'],
+  quantity?: number,
+) => addCartItem(parseProduct(product, variationId, quantity));
+
+export default shoppingCart;
+
+export {
+  totalItems,
+  shoppingCart,
+  addCartItem,
+  removeCartItem,
+  resetCartItems,
+  parseProduct,
+  addProductToCart,
+};
+
+type CartEvent = {
+  addCartItem: CartItem,
+  removeCartItem: CartItem,
+};
+const cartEmitter = mitt<CartEvent>();
+const cloneItems = () => shoppingCart.items.map((item) => ({ ...item }));
+let oldItems = cloneItems();
+const emitCartEvents = useDebounceFn((items: CartItem[]) => {
+  ['addCartItem' as const, 'removeCartItem' as const].forEach((evName) => {
+    const isAdd = evName === 'addCartItem';
+    const baseItems = isAdd ? items : oldItems;
+    const compareItems = isAdd ? oldItems : items;
+    baseItems.forEach((baseItem) => {
+      if (!baseItem.quantity) return;
+      const compareItem = compareItems.find(({ _id }) => _id === baseItem._id);
+      const compareQnt = compareItem?.quantity || 0;
+      if (baseItem.quantity > compareQnt) {
+        cartEmitter.emit(evName, {
+          ...baseItem,
+          quantity: baseItem.quantity - compareQnt,
+        });
+      }
+    });
+  });
+  oldItems = cloneItems();
+}, 200);
+
+watch(shoppingCart.items, (items) => {
+  items.forEach((item) => {
     let finalPrice = item.kit_product?.price && item.kit_product.pack_quantity
       ? item.kit_product.price / item.kit_product.pack_quantity
       : item.price;
@@ -40,93 +108,12 @@ const cartItems = computed(() => {
     } else if (max && item.quantity > max) {
       item.quantity = max;
     }
-    return item;
   });
-});
-const subtotal = computed(() => {
-  return cartItems.value.reduce((acc, item) => {
+  shoppingCart.subtotal = items.reduce((acc, item) => {
     return acc + (item.quantity * (item.final_price || item.price));
   }, 0);
+  emitCartEvents(items);
 });
-const totalItems = computed(() => {
-  return cartItems.value.reduce((acc, item) => {
-    return acc + item.quantity;
-  }, 0);
-});
-const shoppingCart = computed({
-  get() {
-    return {
-      ...cart,
-      subtotal: subtotal.value,
-    };
-  },
-  set(newCart) {
-    Object.assign(cart, newCart, {
-      subtotal: subtotal.value,
-    });
-  },
-});
-
-const addCartItem = (newItem: CartItem) => {
-  const cartObj = shoppingCart.value;
-  const upsertedItem = addItem(cartObj, newItem);
-  if (upsertedItem) {
-    // Force reactivity
-    shoppingCart.value.items = cartObj.items;
-  }
-};
-const removeCartItem = (itemId: string) => {
-  for (let i = 0; i < shoppingCart.value.items.length; i++) {
-    const item = shoppingCart.value.items[i];
-    if (item._id === itemId) {
-      shoppingCart.value.items.splice(i, 1);
-      break;
-    }
-  }
-};
-const addProductToCart = (
-  product: (Partial<Products> | Partial<SearchItem>) & { _id: Products['_id'] },
-  variationId?: Products['_id'],
-  quantity?: number,
-) => addCartItem(parseProduct(product, variationId, quantity));
-
-export default shoppingCart;
-
-export {
-  totalItems,
-  shoppingCart,
-  addCartItem,
-  removeCartItem,
-  parseProduct,
-  addProductToCart,
-};
-
-type CartEvent = {
-  addCartItem: CartItem,
-  removeCartItem: CartItem,
-};
-const cartEmitter = mitt<CartEvent>();
-const cloneItems = () => shoppingCart.value.items.map((item) => ({ ...item }));
-let oldItems = cloneItems();
-watchDebounced(shoppingCart, ({ items }) => {
-  ['addCartItem' as const, 'removeCartItem' as const].forEach((evName) => {
-    const isAdd = evName === 'addCartItem';
-    const baseItems = isAdd ? items : oldItems;
-    const compareItems = isAdd ? oldItems : items;
-    baseItems.forEach((baseItem) => {
-      if (!baseItem.quantity) return;
-      const compareItem = compareItems.find(({ _id }) => _id === baseItem._id);
-      const compareQnt = compareItem?.quantity || 0;
-      if (baseItem.quantity > compareQnt) {
-        cartEmitter.emit(evName, {
-          ...baseItem,
-          quantity: baseItem.quantity - compareQnt,
-        });
-      }
-    });
-  });
-  oldItems = cloneItems();
-}, { debounce: 200 });
 
 export const cartEvents = {
   on: cartEmitter.on,
