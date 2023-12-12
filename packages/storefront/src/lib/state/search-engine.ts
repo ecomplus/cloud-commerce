7c45cc731b241438b1a6367ac2af62117f906323
Leonardo Matos <leomp120894@gmail.com>
2023-12-12 16:15:48 -0300

    feat(storefront): New properties on `SearchEngine` to control fetching state and promises
    Converting all readonly promise states to Vue computeds (getters)

--- packages/storefront/src/lib/state/search-engine.ts
+++ packages/storefront/src/lib/state/search-engine.ts
@@ -1,5 +1,10 @@
 import type { SearchItem, SearchResult } from '@cloudcommerce/types';
-import { ref, watch, shallowReactive } from 'vue';
+import {
+  ref,
+  computed,
+  watch,
+  shallowReactive,
+} from 'vue';
 import { useDebounceFn } from '@vueuse/core';
 import api from '@cloudcommerce/api';
 import useStorage from '@@sf/state/use-storage';
@@ -67,7 +72,15 @@ export class SearchEngine {
   params = shallowReactive<Record<string, any>>({});
   pageSize = ref(24);
   pageNumber = ref(1);
-  isFetching = ref(false);
+  #isFetching = ref(false);
+  isFetching = computed(() => this.#isFetching.value);
+  #wasFetched = ref(false);
+  wasFetched = computed(() => this.#wasFetched.value);
+  #fetching = ref<Promise<void> | null>(null);
+  #fulfillFetching: (() => void) | undefined;
+  fetching = computed(() => this.#fetching);
+  #fetchError = ref<Error | null>(null);
+  fetchError = computed(() => this.#fetchError);
   products = shallowReactive<SearchItem[]>([]);
   meta = shallowReactive<SearchResult<'v1'>['meta']>({
     offset: 0,
@@ -86,11 +99,14 @@ export class SearchEngine {
   } = {}) {
     this.fields = fields;
     this.#search = useDebounceFn((opts) => {
-      this.isFetching.value = true;
+      this.#isFetching.value = true;
       return search(opts);
     }, debounce);
-    watch([this.term, this.params], () => {
+    watch([this.term, this.params, this.pageSize], () => {
       this.pageNumber.value = 1;
+    });
+    watch(this.pageNumber, () => {
+      this.#wasFetched.value = false;
     });
   }
 
@@ -101,25 +117,43 @@ export class SearchEngine {
     }
     const limit = this.pageSize.value;
     const offset = limit * (this.pageNumber.value - 1);
-    const response = await this.#search({
-      term: this.term.value,
-      params: {
-        ...this.params,
-        limit,
-        offset,
-        count: this.isWithCount.value || undefined,
-        buckets: this.isWithBuckets.value || undefined,
-      },
-      fields: this.fields,
-    });
+    if (!this.#fetching.value) {
+      this.#fetching.value = new Promise((resolve) => {
+        this.#fulfillFetching = resolve;
+      });
+    }
+    let response: Awaited<ReturnType<typeof search>> | null | undefined;
+    try {
+      response = await this.#search({
+        term: this.term.value,
+        params: {
+          ...this.params,
+          limit,
+          offset,
+          count: this.isWithCount.value || undefined,
+          buckets: this.isWithBuckets.value || undefined,
+        },
+        fields: this.fields,
+      });
+    } catch (err: any) {
+      if (this.#fulfillFetching) {
+        this.#fetchError.value = err;
+        this.#fulfillFetching();
+      }
+      throw err;
+    }
     if (response) {
-      this.isFetching.value = false;
+      this.#isFetching.value = false;
+      this.#wasFetched.value = true;
       const { data } = response;
       if (data.meta) {
         this.products.splice(0);
         Object.assign(this.meta, data.meta);
       }
       data.result.forEach((item) => this.products.push(item));
+      if (this.#fulfillFetching) {
+        this.#fulfillFetching();
+      }
     }
   }
 }
