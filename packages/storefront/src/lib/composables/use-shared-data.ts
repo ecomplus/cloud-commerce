b0cffcd93a4b4722f3a4c15dddf195d315738fb6
Leonardo Matos <leomp120894@gmail.com>
2023-12-14 12:43:17 -0300

    perf(storefront): Update `useSharedData` preventing unecessary stringify
    BREAKING CHANGE: Editing `useSharedData` return, replacing `inlineClientJs` with `getInlineClientJs` function for just-in-case execution
    
    Minor fixing prop value and return value typedef

--- packages/storefront/src/lib/composables/use-shared-data.ts
+++ packages/storefront/src/lib/composables/use-shared-data.ts
@@ -3,16 +3,17 @@ import { EventEmitter } from 'node:events';
 
 export type Props<T extends string = string> = {
   field: T;
-  value?: T extends 'categories' ? null | Partial<Categories>[]
-    : T extends 'brands' ? null | Partial<Brands>[]
+  value?: T extends 'categories' ? Partial<Categories>[]
+    : T extends 'brands' ? Partial<Brands>[]
     : any;
   timeout?: number;
 }
 
 const emitter = new EventEmitter();
 
-const useSharedData = async <T extends string = string>
-({ field, value, timeout = 1000 }: Props<T>) => {
+const useSharedData = async <T extends string = string>(props: Props<T>) => {
+  const { field, timeout = 1000 } = props;
+  let value: typeof props.value | null = props.value;
   const $data = global.$storefront.data;
   if (value) {
     $data[field] = value;
@@ -27,7 +28,7 @@ const useSharedData = async <T extends string = string>
         clearTimeout(timer);
       };
       const abort = () => {
-        resolve(undefined);
+        resolve(null);
         emitter.removeListener(field, callback);
       };
       const timer = setTimeout(abort, timeout);
@@ -36,7 +37,7 @@ const useSharedData = async <T extends string = string>
   }
   return {
     value,
-    inlineClientJS: `
+    getInlineClientJS: () => `
 window.$storefront.data['${field}'] = ${JSON.stringify(value)};`,
   };
 };
