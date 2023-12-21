9e170e88afaff6a38cb6e68cef33946b91db5f63
Leonardo Matos <leomp120894@gmail.com>
2023-12-20 22:01:09 -0300

    feat(storefront): Abstract handling `sortOption` (optionally) on search showcase composable
    With support for URL params and new `canUseUrlParams` prop
    
    BREAKING CHANGE: Renaming `searchMeta` prop&return to `resultMeta`

--- packages/storefront/src/lib/composables/use-search-showcase.ts
+++ packages/storefront/src/lib/composables/use-search-showcase.ts
@@ -1,5 +1,6 @@
 import type { SearchItem } from '@cloudcommerce/types';
 import { ref, watch, shallowReactive } from 'vue';
+import { useUrlSearchParams } from '@vueuse/core';
 import {
   i19discount,
   i19highestPrice,
@@ -14,16 +15,18 @@ import { SearchEngine } from '@@sf/state/search-engine';
 type SearchEngineInstance = InstanceType<typeof SearchEngine>;
 
 export interface Props {
-  searchEngine?: SearchEngineInstance;
   term?: string | null;
   params?: SearchEngineInstance['params'];
   products?: SearchItem[];
-  searchMeta?: SearchEngineInstance['meta'];
+  resultMeta?: SearchEngineInstance['meta'];
   ssrError?: string | null;
+  canUseUrlParams?: boolean;
 }
 
 const useSearchShowcase = (props: Props) => {
-  let { term, searchEngine } = props;
+  let { term } = props;
+  const canUseUrlParams = props.canUseUrlParams !== false;
+  const urlParams = canUseUrlParams ? useUrlSearchParams('history') : {};
   if (props.ssrError && !import.meta.env.SSR) {
     console.error(new Error(`SSR search error: ${props.ssrError}`));
     if (window.location.pathname.startsWith('/s/')) {
@@ -31,35 +34,32 @@ const useSearchShowcase = (props: Props) => {
     }
   }
   const products = shallowReactive<SearchItem[]>(props.products || []);
-  if (!searchEngine) {
-    searchEngine = new SearchEngine({ debounce: 50 });
-    if (term === undefined && !import.meta.env.SSR) {
-      term = new URLSearchParams(window.location.search).get('q') || null;
-    }
-    if (term !== undefined) {
-      searchEngine.term.value = term;
-    }
-    if (props.params) {
-      Object.assign(searchEngine.params, props.params);
-    }
+  const searchEngine = new SearchEngine({ debounce: 50 });
+  if (term === undefined && !import.meta.env.SSR) {
+    term = new URLSearchParams(window.location.search).get('q') || null;
   }
+  if (term !== undefined) {
+    searchEngine.term.value = term;
+  }
+  Object.assign(searchEngine.params, props.params, urlParams);
   if (!searchEngine.wasFetched.value && !props.products) {
     searchEngine.fetch().catch(console.error);
   }
   searchEngine.isWithCount.value = true;
   searchEngine.isWithBuckets.value = true;
-  const searchMeta = ref({
+  const resultMeta = ref({
     count: 0,
-    ...(props.searchMeta || searchEngine.meta),
+    ...(props.resultMeta || searchEngine.meta),
   });
   watch(searchEngine.products, () => {
     products.splice(0);
-    searchEngine!.products.forEach((item) => products.push(item));
-    searchMeta.value = {
+    searchEngine.products.forEach((item) => products.push(item));
+    resultMeta.value = {
       count: 0,
-      ...searchEngine!.meta,
+      ...searchEngine.meta,
     };
   });
+
   const sortOptions = [
     {
       value: null,
@@ -84,13 +84,28 @@ const useSearchShowcase = (props: Props) => {
       label: i19name,
     },
   ];
+  const sortOption = ref<string | null>(null);
+  watch(sortOption, () => {
+    searchEngine.params.sort = sortOption.value || undefined;
+    searchEngine.fetch();
+  });
+  if (canUseUrlParams) {
+    if (typeof urlParams.sort === 'string' && urlParams.sort) {
+      sortOption.value = urlParams.sort;
+    }
+    watch(searchEngine.params, (params) => {
+      Object.assign(urlParams, params);
+    });
+  }
+
   return {
     searchEngine,
     fetching: searchEngine.fetching.value,
     isFetching: searchEngine.isFetching,
     products,
-    searchMeta,
+    resultMeta,
     sortOptions,
+    sortOption,
   };
 };
 
