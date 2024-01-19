a1896ad1bcf28a6ad17c4806e06a959808050356
Leonardo Matos <leomp120894@gmail.com>
2024-01-19 16:42:20 -0300

    perf(eslint): Moving Vue ESLint plugin to `storefront.eslintrc` only

--- packages/eslint/storefront.eslintrc.cjs
+++ packages/eslint/storefront.eslintrc.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     './base.eslintrc.cjs',
+    'plugin:vue/vue3-essential',
     'plugin:astro/recommended',
   ],
   rules: {
@@ -19,6 +20,7 @@ module.exports = {
     'import/no-dynamic-require': 'off',
     'import/prefer-default-export': 'warn',
     'import/extensions': 'off',
+    'vue/no-multiple-template-root': 'off',
     'vue/multi-word-component-names': ['error', {
       ignores: [
         'Fade',
