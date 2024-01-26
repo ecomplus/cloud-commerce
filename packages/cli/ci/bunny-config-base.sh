5e525ecd94589221886421bede6ab8901ad3c46f
Leonardo Matos <leomp120894@gmail.com>
2024-01-25 21:09:58 -0300

    feat(cli): New `ci/bunny-config-base.sh` script to fix bunny.net CDN pull zone basic configs

---
+++ packages/cli/ci/bunny-config-base.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+if [ -z "$BUNNYNET_API_KEY" ]; then
+  echo "BUNNYNET_API_KEY env var must be set"
+  exit 1
+fi
+if [ $# -eq 0 ]; then
+  echo "Provide the domain as the first argument"
+  exit 1
+fi
+
+domain=$1
+
+response=$(curl --silent --request GET \
+  --url https://api.bunny.net/pullzone \
+  --header "AccessKey: $BUNNYNET_API_KEY" \
+  --header 'accept: application/json')
+
+pull_zone_id=$(echo $response | jq --arg domain "$domain" '[.[] | select(.Hostnames[].Value | contains($domain)).Id][0]')
+if [ -z "$pull_zone_id" ]; then
+  echo "bunny.net pull zone not found for domain $domain"
+  exit 1
+fi
+
+edge_rules=$(echo $response | jq --arg id "$pull_zone_id" '.[] | select(.Id == ($id | tonumber)).EdgeRules')
+
+curl --silent --request POST \
+  --url "https://api.bunny.net/pullzone/$pull_zone_id" \
+  --header "AccessKey: $BUNNYNET_API_KEY" \
+  --header 'accept: application/json' \
+  --header 'content-type: application/json' \
+  --data '
+{
+  "EnableGeoZoneUS": true,
+  "EnableGeoZoneEU": true,
+  "EnableGeoZoneASIA": false,
+  "EnableGeoZoneSA": true,
+  "EnableGeoZoneAF": false,
+  "ZoneSecurityEnabled": false,
+  "ZoneSecurityIncludeHashRemoteIP": false,
+  "IgnoreQueryStrings": true,
+  "MonthlyBandwidthLimit": 0,
+  "AddHostHeader": false,
+  "OriginHostHeader": "",
+  "Type": 0,
+  "AccessControlOriginHeaderExtensions": [
+    "eot",
+    "ttf",
+    "woff",
+    "woff2",
+    "css"
+  ],
+  "EnableAccessControlOriginHeader": true,
+  "DisableCookies": false,
+  "EnableOriginShield": true,
+  "CacheControlMaxAgeOverride": -1,
+  "CacheControlPublicMaxAgeOverride": -1,
+  "BurstSize": 0,
+  "RequestLimit": 0,
+  "BlockRootPathAccess": false,
+  "BlockPostRequests": false,
+  "LimitRatePerSecond": 0,
+  "LimitRateAfter": 0,
+  "ConnectionLimitPerIPCount": 0,
+  "AddCanonicalHeader": false,
+  "EnableLogging": true,
+  "EnableCacheSlice": false,
+  "EnableSmartCache": false,
+  "EnableWebPVary": false,
+  "EnableAvifVary": false,
+  "EnableCountryCodeVary": false,
+  "EnableMobileVary": false,
+  "EnableCookieVary": true,
+  "CookieVaryParameters": [
+    "bid",
+    "branch"
+  ],
+  "EnableHostnameVary": false,
+  "LoggingIPAnonymizationEnabled": true,
+  "EnableTLS1": true,
+  "EnableTLS1_1": true,
+  "VerifyOriginSSL": false,
+  "ErrorPageEnableCustomCode": false,
+  "ErrorPageEnableStatuspageWidget": false,
+  "ErrorPageWhitelabel": false,
+  "OriginShieldZoneCode": "IL",
+  "LogForwardingEnabled": false,
+  "LoggingSaveToStorage": false,
+  "FollowRedirects": false,
+  "PermaCacheStorageZoneId": 0,
+  "OriginRetries": 0,
+  "OriginConnectTimeout": 5,
+  "OriginResponseTimeout": 15,
+  "UseStaleWhileUpdating": true,
+  "UseStaleWhileOffline": true,
+  "OriginRetry5XXResponses": false,
+  "OriginRetryConnectionTimeout": true,
+  "OriginRetryResponseTimeout": true,
+  "OriginRetryDelay": 0,
+  "QueryStringVaryParameters": [],
+  "OriginShieldEnableConcurrencyLimit": false,
+  "OriginShieldMaxConcurrentRequests": 200,
+  "EnableSafeHop": false,
+  "CacheErrorResponses": false,
+  "OriginShieldQueueMaxWaitTime": 30,
+  "OriginShieldMaxQueuedRequests": 5000,
+  "UseBackgroundUpdate": true,
+  "EnableAutoSSL": false,
+  "EnableQueryStringOrdering": true,
+  "LogAnonymizationType": 0,
+  "LogFormat": 1,
+  "LogForwardingFormat": 1,
+  "ShieldDDosProtectionType": 1,
+  "ShieldDDosProtectionEnabled": false,
+  "EnableRequestCoalescing": true,
+  "RequestCoalescingTimeout": 30,
+  "DisableLetsEncrypt": false,
+  "EnableBunnyImageAi": false,
+  "PreloadingScreenEnabled": false,
+  "RoutingFilters": [
+    "all"
+  ]
+}
+'
+printf "\n\n> Configured pull zone \"$pull_zone_id\"\n"
+
+configure_edge_rule() {
+  printf "\n"
+  local description=$1
+  local rule_data=$2
+  local found_rule=$(echo $edge_rules | jq --arg description "$description" '.[] | select(.Description == $description)')
+  local guid=$(echo $found_rule | jq -r '.Guid // empty')
+
+  local json_data="$rule_data"
+  if [ -n "$guid" ]; then
+    json_data=$(echo $json_data | jq --arg guid "$guid" '. + {Guid: $guid}')
+  fi
+
+  curl --silent --request POST \
+    --url "https://api.bunny.net/pullzone/$pull_zone_id/edgerules/addOrUpdate" \
+    --header "AccessKey: $BUNNYNET_API_KEY" \
+    --header 'content-type: application/json' \
+    --data "$json_data"
+  printf "\n\n> Configured edge rule \"$description\"\n"
+}
+
+configure_edge_rule "APIs bypass CDN cache" '
+{
+  "ActionType": 3,
+  "ActionParameter1": "0",
+  "ActionParameter2": "",
+  "Triggers": [
+    {
+      "Type": 0,
+      "PatternMatches": [
+        "*/_api/*",
+        "*/_feeds/*",
+        "*/.*/git/*",
+        "*/_analytics"
+      ],
+      "PatternMatchingType": 0,
+      "Parameter1": ""
+    }
+  ],
+  "TriggerMatchingType": 0,
+  "Description": "APIs bypass CDN cache",
+  "Enabled": true
+}
+'
