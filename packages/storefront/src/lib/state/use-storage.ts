77bffb9cc1bb6430cb1b4d038546e5af67cc56eb
Leonardo Matos <leomp120894@gmail.com>
2024-02-05 17:35:59 -0300

    fix(storefront): Sync browser storage states between tabs with `BroadcastChannel`
    New optional `isWithBroadcast = true` param to `useStorage` state primitive

--- packages/storefront/src/lib/state/use-storage.ts
+++ packages/storefront/src/lib/state/use-storage.ts
@@ -5,23 +5,37 @@ const useStorage = <T extends {}>(
   key: string,
   initialValue: T,
   storage = globalThis.localStorage,
+  isWithBroadcast = true,
 ) => {
   if (!storage) {
     return reactive<T>(initialValue);
   }
-  let persistedValue: T | undefined | null;
-  const sessionJson = storage.getItem(key);
-  if (sessionJson) {
-    try {
-      persistedValue = JSON.parse(sessionJson);
-    } catch (e) {
-      persistedValue = null;
-      storage.removeItem(key);
+  // eslint-disable-next-line consistent-return
+  const getStorageItem = () => {
+    const sessionJson = storage.getItem(key);
+    if (sessionJson) {
+      try {
+        return JSON.parse(sessionJson) as T;
+      } catch (e) {
+        storage.removeItem(key);
+        return null;
+      }
     }
-  }
+  };
+  const persistedValue = getStorageItem();
   const state = reactive<T>(persistedValue || initialValue);
+  let bc: BroadcastChannel | undefined;
+  if (isWithBroadcast && !import.meta.env.SSR && globalThis.BroadcastChannel) {
+    bc = new BroadcastChannel(key);
+    bc.onmessage = (event) => {
+      if (event.data === 'set') {
+        Object.assign(state, getStorageItem());
+      }
+    };
+  }
   watchDebounced(state, () => {
     storage.setItem(key, JSON.stringify(state));
+    if (bc) bc.postMessage('set');
   }, {
     debounce: 50,
   });
