5360ed1bb5797b10f37f6b1b5020620dcffdf00f
Leonardo Matos <leomp120894@gmail.com>
2024-03-27 15:57:22 -0300

    feat(passport): Handle `identify` endpoint (from v1) for low level login returning masked info
    For checkout with email & doc number "login"

--- packages/passport/src/firebase/serve-passport-api.ts
+++ packages/passport/src/firebase/serve-passport-api.ts
@@ -1,7 +1,51 @@
 import type { Request, Response } from 'firebase-functions';
-import type { ApiError } from '@cloudcommerce/api';
+import type { Customers, ApiError } from '@cloudcommerce/api/types';
 import { logger } from 'firebase-functions';
-import authenticateCustomer from './authenticate-customer';
+import api from '@cloudcommerce/api';
+import { findCustomerByEmail, authenticateCustomer } from './authenticate-customer';
+
+const maskCustomerFields = (customer: Customers) => {
+  const safeCustomer: Customers = {
+    _id: customer._id,
+    store_id: customer.store_id,
+    created_at: customer.created_at,
+    updated_at: customer.updated_at,
+    main_email: customer.main_email, // email already used to identify the customer
+    display_name: customer.display_name,
+    name: customer.name && {
+      given_name: customer.name.given_name,
+      family_name: '***',
+    },
+    state: customer.state,
+    enabled: customer.enabled,
+    login: customer.login,
+    locale: customer.locale,
+    accepts_marketing: customer.accepts_marketing,
+    birth_date: customer.birth_date,
+    pronoun: customer.pronoun,
+    currency_id: customer.currency_id,
+    currency_symbol: customer.currency_symbol,
+    favorites: customer.favorites,
+    last_searched_terms: customer.last_searched_terms,
+    last_visited_products: customer.last_visited_products,
+    phones: customer.phones?.map((phone) => ({
+      ...phone,
+      number: '***' + phone.number.substring(phone.number.length - 2),
+    })),
+    addresses: customer.addresses?.map((address) => ({
+      zip: address.zip,
+      province_code: address.province_code,
+      line_address: `${(address.street || address.city)} ***`,
+      name: '***',
+      default: address.default,
+    })),
+    orders: customer.orders?.map((order) => ({
+      _id: order._id,
+      number: order.number,
+    })),
+  };
+  return safeCustomer;
+};
 
 export default async (req: Request, res: Response) => {
   let { url } = req;
@@ -9,7 +53,53 @@ export default async (req: Request, res: Response) => {
     url = url.slice(0, -5);
   }
   url = url.replace('/_api/passport', ''); // due to hosting rewrite
-  const endpoint = url.split('/')[1];
+  const endpoint = url.split('/').pop();
+
+  if (endpoint === 'identify') {
+    const { body } = req;
+    if (body) {
+      const email = body.email;
+      if (typeof email === 'string' && /^[^@]+@[^@]+\.[^@]+$/.test(email)) {
+        const docNumber = body.doc_number ? `${body.doc_number}` : undefined;
+        try {
+          const customerMatch = await findCustomerByEmail(email, docNumber);
+          if (customerMatch) {
+            const customerId = customerMatch._id;
+            const customer: Partial<Customers> = {
+              _id: customerId,
+              main_email: email,
+              doc_number: docNumber,
+            };
+            const level = docNumber ? 1 : 0;
+            if (level) {
+              Object.assign(
+                customer,
+                maskCustomerFields((await api.get(`customers/${customerId}`)).data),
+              );
+            }
+            res.send({
+              customer,
+              auth: {
+                id: customer._id,
+                level,
+                token: null,
+              },
+            });
+            return;
+          }
+          res.status(403).json({
+            status: 403,
+            error: 'Forbidden, no profile found with email provided',
+          });
+        } catch (err) {
+          logger.error(err);
+          res.sendStatus((err as ApiError).statusCode || 500);
+        }
+      }
+    }
+    res.sendStatus(400);
+    return;
+  }
 
   if (endpoint !== 'token') {
     res.sendStatus(404);
