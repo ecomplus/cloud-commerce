4ffbf859bb601f79903c4f5da7733ec3c148d558
Leonardo Matos <leomp120894@gmail.com>
2024-04-06 17:08:12 -0300

    perf(passport): Deploy passport to Functions v2 for concurrency (24)

--- packages/passport/src/firebase.ts
+++ packages/passport/src/firebase.ts
@@ -1,30 +1,30 @@
-import functions from 'firebase-functions/v1';
+/* eslint-disable import/prefer-default-export */
+import { onRequest } from 'firebase-functions/v2/https';
 import config from '@cloudcommerce/firebase/lib/config';
 import servePassportApi from './firebase/serve-passport-api';
 
-const { httpsFunctionOptions } = config.get();
-const { region } = httpsFunctionOptions;
+const {
+  httpsFunctionOptions: { region },
+} = config.get();
 
-// eslint-disable-next-line import/prefer-default-export
-export const passport = functions
-  .region(region)
-  .runWith({
-    ...httpsFunctionOptions,
-    timeoutSeconds: 30,
-  })
-  .https.onRequest((req, res) => {
-    if (!process.env.PASSPORT_DISABLE_CORS) {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
-      res.setHeader('Access-Control-Max-Age', '600');
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Authorization, Content-Type, X-Store-ID, X-My-ID, X-Access-Token',
-      );
-      if (req.method === 'OPTIONS') {
-        res.end();
-        return Promise.resolve();
-      }
+export const passport = onRequest({
+  concurrency: 24,
+  region,
+  memory: '256MiB',
+  timeoutSeconds: 30,
+}, (req, res) => {
+  if (!process.env.PASSPORT_DISABLE_CORS) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, OPTIONS');
+    res.setHeader('Access-Control-Max-Age', '600');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Authorization, Content-Type, X-Store-ID, X-My-ID, X-Access-Token',
+    );
+    if (req.method === 'OPTIONS') {
+      res.end();
+      return;
     }
-    return servePassportApi(req, res);
-  });
+  }
+  servePassportApi(req, res);
+});
