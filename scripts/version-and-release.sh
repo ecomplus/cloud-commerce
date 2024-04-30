7824dbda0e1e3291d2c1152e2a9893e9ef8f243c
Leonardo Matos <leomp120894@gmail.com>
2024-04-29 21:48:10 -0300

    build: Additional commit to ensure package versions commited on release

--- scripts/version-and-release.sh
+++ scripts/version-and-release.sh
@@ -4,4 +4,7 @@ pnpm build --filter='@cloudcommerce/*' || exit 1
 pnpm test || exit 1
 pnpm run -r prerelease
 npx standard-version --commit-all
+sleep 1
+(git add packages/**/package.json package.json pnpm-lock.yaml CHANGELOG.md \
+  && git commit -m 'chore: Fixing package versions post-release') || true
 npx zx scripts/release.mjs --publish
