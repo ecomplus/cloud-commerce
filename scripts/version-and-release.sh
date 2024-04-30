7966f5cbaf01d95c56394e4005001ca614d573ea
Leonardo Matos <leomp120894@gmail.com>
2024-04-30 12:06:03 -0300

    build: Sleep on release scripts to try waiting concurrent git processes

--- scripts/version-and-release.sh
+++ scripts/version-and-release.sh
@@ -3,5 +3,7 @@
 pnpm build --filter='@cloudcommerce/*' || exit 1
 pnpm test || exit 1
 pnpm run -r prerelease
+sleep 1
 npx standard-version --commit-all || exit 1
+sleep 1
 npx zx scripts/release.mjs --publish
