5dc2ac091342733b8efcd9f2b833d49f8245f15e
Leonardo Matos <leomp120894@gmail.com>
2024-04-30 08:29:13 -0300

    build: Fail release script if `standard-versions` ends with error

--- scripts/version-and-release.sh
+++ scripts/version-and-release.sh
@@ -3,5 +3,5 @@
 pnpm build --filter='@cloudcommerce/*' || exit 1
 pnpm test || exit 1
 pnpm run -r prerelease
-npx standard-version --commit-all
+npx standard-version --commit-all || exit 1
 npx zx scripts/release.mjs --publish
