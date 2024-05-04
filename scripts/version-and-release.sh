dd918262eff0ce78073a5284af137fc97aec184d
Leonardo Matos <leomp120894@gmail.com>
2024-05-04 16:03:46 -0300

    build: Try fixing release script explicitly `rm -f .git/index.lock`

--- scripts/version-and-release.sh
+++ scripts/version-and-release.sh
@@ -3,6 +3,7 @@
 pnpm build --filter='@cloudcommerce/*' || exit 1
 pnpm test || exit 1
 pnpm run -r prerelease
+rm -f .git/index.lock
 npx standard-version || exit 1
 sleep 1
 (git add packages/**/package.json package.json pnpm-lock.yaml CHANGELOG.md \
