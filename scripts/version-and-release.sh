eb615322a32b79b821bff1576df53b3db8a6cd14
Leonardo Matos <leomp120894@gmail.com>
2024-05-05 22:58:18 -0300

    build: Try fixing release script with (big) sleep before version bump

--- scripts/version-and-release.sh
+++ scripts/version-and-release.sh
@@ -5,7 +5,7 @@ pnpm test || exit 1
 pnpm run -r prerelease
 sleep 5
 rm -f .git/index.lock
-npx standard-version || exit 1
+npx standard-version
 sleep 1
 (git add packages/**/package.json package.json pnpm-lock.yaml CHANGELOG.md \
   && git commit -m 'chore: Fixing package versions post-release') || true
