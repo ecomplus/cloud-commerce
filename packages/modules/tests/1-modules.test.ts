73822a88ef80a53cb0dcfc3bdb44c431c4e0b650
Wisley Alves <wisley7lgithub@gmail.com>
2024-07-05 17:12:46 -0300

    fix(tests-modules): Updatetest to vitest

--- packages/modules/tests/1-modules.test.ts
+++ packages/modules/tests/1-modules.test.ts
@@ -1,26 +1,25 @@
-import assert from 'node:assert';
-import test, { describe } from 'node:test';
+import { describe, test, expect } from 'vitest';
 import { modulesUrl } from '@cloudcommerce/test-base';
 
 describe('Test Schemas', async () => {
   test('@checkout', async () => {
     const req = await fetch(`${modulesUrl}/@checkout/schema`);
-    assert.strictEqual(req.status, 200);
+    expect(req.status).toBe(200);
   });
   test('apply_discount', async () => {
     const req = await fetch(`${modulesUrl}/apply_discount/schema`);
-    assert.strictEqual(req.status, 200);
+    expect(req.status).toBe(200);
   });
   test('calculate_shipping', async () => {
     const req = await fetch(`${modulesUrl}/calculate_shipping/schema`);
-    assert.strictEqual(req.status, 200);
+    expect(req.status).toBe(200);
   });
   test('create_transaction', async () => {
     const req = await fetch(`${modulesUrl}/create_transaction/schema`);
-    assert.strictEqual(req.status, 200);
+    expect(req.status).toBe(200);
   });
   test('list_payments', async () => {
     const req = await fetch(`${modulesUrl}/list_payments/schema`);
-    assert.strictEqual(req.status, 200);
+    expect(req.status).toBe(200);
   });
 });
