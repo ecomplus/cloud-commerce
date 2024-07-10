467e6045aca59fbb2c611532befefbf4f95c481a
Leonardo Matos <leomp120894@gmail.com>
2024-07-10 16:41:52 -0300

    chore(storefront): Creating symlink from store src/sections on monorepo

--- packages/storefront/scripts/prepare-monorepo.sh
+++ packages/storefront/scripts/prepare-monorepo.sh
@@ -5,6 +5,7 @@ rm -rf public && ln -s ../../store/functions/ssr/public public
 rm -rf src/pages && ln -s ../../../store/functions/ssr/src/pages src/pages
 rm -rf src/layouts && ln -s ../../../store/functions/ssr/src/layouts src/layouts
 rm -rf src/main && ln -s ../../../store/functions/ssr/src/main src/main
+rm -rf src/sections && ln -s ../../../store/functions/ssr/src/sections src/sections
 rm -rf src/components && ln -s ../../../store/functions/ssr/src/components src/components
 rm -rf src/scripts && ln -s ../../../store/functions/ssr/src/scripts src/scripts
 rm -rf src/assets && ln -s ../../../store/functions/ssr/src/assets src/assets
