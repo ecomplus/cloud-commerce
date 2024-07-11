1ff2282fc92d8f50507fb61250c6b3234ae87216
Leonardo Matos <leomp120894@gmail.com>
2024-07-11 18:28:22 -0300

    chore(storefront): Reverting `valibot` implementation
    Just a generic will be needed to infer prop types from CMS fields output

--- packages/storefront/src/lib/composables/use-banner.ts
+++ packages/storefront/src/lib/composables/use-banner.ts
@@ -1,20 +1,16 @@
-import type { InferInput } from 'valibot';
 import { computed } from 'vue';
-import { looseObject, string, optional } from 'valibot';
 import { parseShippingPhrase } from '@@sf/state/modules-info';
 
-export const BannerProps = looseObject({
-  img: optional(string()),
-  alt: optional(string()),
-  mobileImg: optional(string()),
-  href: optional(string()),
-  title: optional(string()),
-  subtitle: optional(string()),
-  buttonLink: optional(string()),
-  buttonText: optional(string()),
-});
-
-export type Props = InferInput<typeof BannerProps>;
+export type Props = {
+  img?: string;
+  alt?: string;
+  mobileImg?: string;
+  href?: string;
+  title?: string;
+  subtitle?: string;
+  buttonLink?: string;
+  buttonText?: string;
+};
 
 const useBanner = (props: Props) => {
   const parsedTitle = computed(() => {
