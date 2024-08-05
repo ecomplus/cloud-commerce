b7bf8235829c1a97f2cc30cf31c56e82f24e7f9e
Leonardo Matos <leomp120894@gmail.com>
2024-08-05 14:21:30 -0300

    build: Force rule `@typescript-eslint/no-unused-vars: warn` on build lib script

--- scripts/build-lib.sh
+++ scripts/build-lib.sh
@@ -4,4 +4,4 @@ rm -rf lib
 npx tsc --outDir lib
 find lib -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/([ie][mx]port [^']+?)'(\\.[^']+)';/\\1'\\2.js';/g"
 find lib -type f -name "*.js" -print0 | xargs -0 sed -i -r "s/.([cm])js.js/.\\1js/g"
-npx eslint --rule 'max-len: off' --rule 'import/prefer-default-export: off' --ext .js lib --fix
+npx eslint --rule 'max-len: off' --rule 'import/prefer-default-export: off' --rule '@typescript-eslint/no-unused-vars: warn' --ext .js lib --fix
