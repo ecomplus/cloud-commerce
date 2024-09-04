42dfec9547abaafb49522ab7257a6ebf56c50af3
Leonardo Matos <leomp120894@gmail.com>
2024-09-04 18:49:02 -0300

    refactor(storefront): Dropping `$prefetch` and stop prefetching at `<ALink>` at all
    Doesnt solve INP, is costly and CDN response is (really) fast enough

--- packages/storefront/src/lib/components/globals/ALink.vue
+++ packages/storefront/src/lib/components/globals/ALink.vue
@@ -1,22 +1,12 @@
 <script setup lang="ts">
-import {
-  ref,
-  computed,
-  watch,
-  onMounted,
-} from 'vue';
-import { useElementVisibility, useElementHover } from '@vueuse/core';
-import { isMobile, requestIdleCallback } from '@@sf/sf-lib';
+import { ref, computed } from 'vue';
 
 export type Props = {
   href?: string | null;
   target?: string;
-  prefetch?: 'hover' | 'visible' | 'never';
+  prefetch?: string; /* DEPRECATED */
 }
-
-const props = withDefaults(defineProps<Props>(), {
-  prefetch: 'hover',
-});
+const props = defineProps<Props>();
 const link = ref<HTMLElement | null>(null);
 const isExternalLink = computed(() => {
   if (props.href?.startsWith('https:')) {
@@ -43,54 +33,6 @@ const linkTarget = computed(() => {
   if (isExternalLink.value) return '_blank';
   return undefined;
 });
-const prefetchHref = computed(() => {
-  if (import.meta.env.SSR || !props.href || linkTarget.value) return null;
-  const pathname = props.href.replace(/^(https:\/\/[^/]+)?([^#?]+).*$/i, '$2');
-  if (pathname.startsWith('/app/') || pathname === window.location.pathname) {
-    return null;
-  }
-  return pathname;
-});
-if (prefetchHref.value) {
-  const prefetchOnNext = () => {
-    requestIdleCallback(() => {
-      const next = () => {
-        if (!prefetchHref.value) return;
-        window.$prefetch!(prefetchHref.value);
-      };
-      if (!window.$interactionOrAwaken) {
-        window.addEventListener('firstInteraction', next, { once: true });
-      } else {
-        window.$interactionOrAwaken.then(next);
-      }
-    });
-  };
-  onMounted(() => {
-    const { prefetch } = props;
-    let isOnVisible = prefetch === 'visible';
-    let delayMs = isOnVisible ? 100 : 1;
-    if (isMobile && prefetch === 'hover') {
-      isOnVisible = true;
-      delayMs = 800;
-    }
-    const isOnHover = !isOnVisible && prefetch === 'hover';
-    const useElementEv = isOnVisible
-      ? useElementVisibility
-      : isOnHover && useElementHover;
-    if (!useElementEv) return;
-    const isActive = useElementEv(link.value);
-    const unwatch = watch(isActive, (_isActive) => {
-      if (!_isActive || !window.$prefetch) return;
-      unwatch();
-      setTimeout(() => {
-        if (isOnVisible && !isActive.value) return;
-        prefetchOnNext();
-      }, delayMs);
-    }, {
-      immediate: true,
-    });
-  });
-}
 </script>
 
 <template>
