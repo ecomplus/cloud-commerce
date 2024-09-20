ab34c3fbffb8deb2020af290779de4f80503e2f0
Leonardo Matos <leomp120894@gmail.com>
2024-09-20 18:43:54 -0300

    fix(storefront): Prevent hydration mismatch errors with `<SocialNetworkLink>` for whatsapp

--- packages/storefront/src/lib/components/SocialNetworkLink.vue
+++ packages/storefront/src/lib/components/SocialNetworkLink.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { type NetworkName, isMobile, socialNetworks } from '@@sf/sf-lib';
-import { computed } from 'vue';
+import { ref, computed, onMounted } from 'vue';
 import SocialNetworkIcon from '@@sf/components/SocialNetworkIcon.vue';
 
 export type Props = {
@@ -9,19 +9,24 @@ export type Props = {
   message?: string;
 }
 const props = defineProps<Props>();
+const wppService = ref('api');
 const fixedHref = computed<string>(() => {
   let href = props.href || socialNetworks[props.network] || '';
   if (props.network === 'whatsapp') {
     const tel = href.replace(/[^+\d]/g, '');
-    const wppService = isMobile || import.meta.env.SSR ? 'api' : 'web';
     // eslint-disable-next-line prefer-template
-    href = `https://${wppService}.whatsapp.com/send?phone=`
+    href = `https://${wppService.value}.whatsapp.com/send?phone=`
       + encodeURIComponent(tel.charAt(0) === '+' ? tel : `+55${tel}`);
     if (props.message) {
       href += `&text=${encodeURIComponent(props.message)}`;
     }
   }
   return href;
+});
+onMounted(() => {
+  if (!isMobile) {
+    wppService.value = 'web';
+  }
 });
 </script>
 
