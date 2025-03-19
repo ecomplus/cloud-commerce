092cc7e400537bdbe42e9cb4d4039cba99e39fa1
Leonardo Matos <leomp120894@gmail.com>
2025-03-19 04:39:18 -0300

    fix(storefront): Properly parsing variation price and other fields on add to cart

--- packages/storefront/src/lib/state/shopping-cart/parse-product.ts
+++ packages/storefront/src/lib/state/shopping-cart/parse-product.ts
@@ -7,35 +7,38 @@ export default (
   variationId?: Products['_id'],
   quantity?: number,
 ) => {
-  if (typeof quantity !== 'number' || Number.isNaN(quantity)) {
-    quantity = product.min_quantity || 1;
-  }
-  const minQuantity = product.min_quantity || 0;
   const variation = variationId && product.variations
     ? product.variations.find(({ _id }) => _id === variationId)
     : undefined;
+  const mergedProduct = { ...product, ...variation };
+  if (typeof quantity !== 'number' || Number.isNaN(quantity)) {
+    quantity = mergedProduct.min_quantity || 1;
+  }
+  const minQuantity = mergedProduct.min_quantity || 0;
   const item: ExtendedCartItem = {
     product_id: product._id,
     variation_id: variationId,
-    sku: variation?.sku || product.sku,
-    name: variation?.name || product.name,
-    slug: product.slug,
-    production_time: variation?.production_time || product.production_time,
-    currency_id: product.currency_id,
-    currency_symbol: product.currency_symbol,
-    base_price: variation?.base_price || product.base_price,
-    max_quantity: product.quantity,
+    sku: mergedProduct.sku,
+    name: mergedProduct.name,
+    slug: mergedProduct.slug,
+    production_time: mergedProduct.production_time,
+    currency_id: mergedProduct.currency_id,
+    currency_symbol: mergedProduct.currency_symbol,
+    base_price: mergedProduct.base_price,
+    max_quantity: mergedProduct.quantity,
     quantity: minQuantity > 0 ? Math.max(minQuantity, quantity) : quantity,
-    price: getPrice(product),
-    categories: product.categories,
+    price: getPrice(mergedProduct),
+    categories: mergedProduct.categories,
   };
-  if (variation?.picture_id && product.pictures) {
-    item.picture = product.pictures.find((_picture) => {
-      return _picture._id === variation.picture_id;
-    });
-  }
-  if (!item.picture && product.pictures) {
-    [item.picture] = product.pictures;
+  if (product.pictures) {
+    if (variation?.picture_id) {
+      item.picture = product.pictures.find((_picture) => {
+        return _picture._id === variation.picture_id;
+      });
+    }
+    if (!item.picture) {
+      [item.picture] = product.pictures;
+    }
   }
   return item;
 };
