a1862804b3f2f6bd45123bea4e1e75bb9847e760
Leonardo Matos <leomp120894@gmail.com>
2025-05-09 22:19:41 -0300

    fix(loyalty-points): Fix setting first points (new array) to new customers
    Ensure no retry (warn only) for not found customer or order

--- packages/apps/loyalty-points/src/functions-lib/handle-loyalty-points-event.ts
+++ packages/apps/loyalty-points/src/functions-lib/handle-loyalty-points-event.ts
@@ -1,4 +1,9 @@
-import type { Orders, Customers, ResourceId } from '@cloudcommerce/types';
+import type {
+  ApiError,
+  ResourceId,
+  Orders,
+  Customers,
+} from '@cloudcommerce/api/types';
 import api from '@cloudcommerce/api';
 import { getFirestore, Timestamp } from 'firebase-admin/firestore';
 import { logger } from '@cloudcommerce/firebase/lib/config';
@@ -89,99 +94,95 @@ const handleLoyaltyPointsEvent = async (
       const { amount, buyers } = order;
       const customerId = buyers && buyers[0] && buyers[0]._id;
       if (customerId) {
-        const pointsList: Customers['loyalty_points_entries'] = (await api.get(
-          `customers/${customerId}/loyalty_points_entries`,
-        )
-        ).data;
+        const pointsList = (await api.get(`customers/${customerId}/loyalty_points_entries`))
+          .data as Customers['loyalty_points_entries'] || [];
 
-        if (pointsList) {
-          const hasEarnedPoints = haveEarnedPoints(pointsList, orderId);
+        const hasEarnedPoints = haveEarnedPoints(pointsList, orderId);
 
-          if (isPaid && !hasEarnedPoints) {
-            const docRef = getFirestore().doc(`pointsToAdd/${orderId}`);
-            const docSnapshot = await docRef.get();
-            if (!docSnapshot.exists) {
-              const pointEntries: any[] = [];
+        if (isPaid && !hasEarnedPoints) {
+          const docRef = getFirestore().doc(`pointsToAdd/${orderId}`);
+          const docSnapshot = await docRef.get();
+          if (!docSnapshot.exists) {
+            const pointEntries: any[] = [];
 
-              for (let i = 0; i < programRules.length; i++) {
-                const rule = programRules[i];
-                if (rule.earn_percentage === 0) {
-                  continue;
-                }
-                if (
-                  !rule.min_subtotal_to_earn
-                    || (amount?.subtotal && rule.min_subtotal_to_earn <= amount.subtotal)
-                ) {
-                  let subtotal = amount.subtotal as number;
+            for (let i = 0; i < programRules.length; i++) {
+              const rule = programRules[i];
+              if (rule.earn_percentage === 0) {
+                continue;
+              }
+              if (
+                !rule.min_subtotal_to_earn
+                  || (amount?.subtotal && rule.min_subtotal_to_earn <= amount.subtotal)
+              ) {
+                let subtotal = amount.subtotal as number;
 
-                  if (Array.isArray(rule.category_ids) && rule.category_ids.length) {
-                    if (!order.items || !order.items.length) {
-                      continue;
-                    }
-
-                    // eslint-disable-next-line no-await-in-loop
-                    const { data: { result } } = await api.get('search/v1', {
-                      limit: order.items.length,
-                      params: {
-                        _id: order.items.map((item) => item.product_id),
-                        'categories._id': rule.category_ids,
-                      },
-                    });
-
-                    // eslint-disable-next-line no-await-in-loop
-                    order.items.forEach((item) => {
-                      if (!result.find(({ _id }) => _id === item.product_id)) {
-                        subtotal -= (ecomUtils.price(item) * item.quantity);
-                      }
-                    });
+                if (Array.isArray(rule.category_ids) && rule.category_ids.length) {
+                  if (!order.items || !order.items.length) {
+                    continue;
                   }
 
-                  const pointsValue = ((rule.earn_percentage || 1) / 100)
-                    * (subtotal - (amount.discount || 0));
+                  // eslint-disable-next-line no-await-in-loop
+                  const { data: { result } } = await api.get('search/v1', {
+                    limit: order.items.length,
+                    params: {
+                      '_id': order.items.map((item) => item.product_id),
+                      'categories._id': rule.category_ids,
+                    },
+                  });
 
-                  if (pointsValue > 0) {
-                    let validThru: string | undefined;
-                    if (rule.expiration_days > 0) {
-                      const d = new Date();
-                      d.setDate(d.getDate() + 2 + rule.expiration_days);
-                      validThru = d.toISOString();
+                  // eslint-disable-next-line no-await-in-loop
+                  order.items.forEach((item) => {
+                    if (!result.find(({ _id }) => _id === item.product_id)) {
+                      subtotal -= (ecomUtils.price(item) * item.quantity);
                     }
-                    const data = {
-                      name: rule.name,
-                      program_id: getProgramId(rule, i),
-                      earned_points: pointsValue,
-                      active_points: pointsValue,
-                      ratio: rule.ratio || 1,
-                      order_id: orderId,
-                    };
-                    if (validThru) {
-                      Object.assign(data, { valid_thru: validThru });
-                    }
-                    pointEntries.push(data);
+                  });
+                }
+
+                const pointsValue = ((rule.earn_percentage || 1) / 100)
+                  * (subtotal - (amount.discount || 0));
+
+                if (pointsValue > 0) {
+                  let validThru: string | undefined;
+                  if (rule.expiration_days > 0) {
+                    const d = new Date();
+                    d.setDate(d.getDate() + 2 + rule.expiration_days);
+                    validThru = d.toISOString();
                   }
+                  const data = {
+                    name: rule.name,
+                    program_id: getProgramId(rule, i),
+                    earned_points: pointsValue,
+                    active_points: pointsValue,
+                    ratio: rule.ratio || 1,
+                    order_id: orderId,
+                  };
+                  if (validThru) {
+                    Object.assign(data, { valid_thru: validThru });
+                  }
+                  pointEntries.push(data);
                 }
               }
-
-              await docRef.set({
-                customerId,
-                pointEntries,
-                queuedAt: Timestamp.now(),
-              });
-
-              return responsePubSub(ECHO_SUCCESS);
             }
-          }
 
-          if (isCancelled && hasEarnedPoints) {
-            for (let i = 0; i < pointsList.length; i++) {
-              if (pointsList[i].order_id === orderId) {
-                // eslint-disable-next-line no-await-in-loop
-                await api.delete(`customers/${customerId}/loyalty_points_entries/${i}`);
-              }
-            }
+            await docRef.set({
+              customerId,
+              pointEntries,
+              queuedAt: Timestamp.now(),
+            });
 
             return responsePubSub(ECHO_SUCCESS);
           }
+        }
+
+        if (isCancelled && hasEarnedPoints) {
+          for (let i = 0; i < pointsList.length; i++) {
+            if (pointsList[i].order_id === orderId) {
+              // eslint-disable-next-line no-await-in-loop
+              await api.delete(`customers/${customerId}/loyalty_points_entries/${i}`);
+            }
+          }
+
+          return responsePubSub(ECHO_SUCCESS);
         }
 
         if (isCancelled) {
@@ -240,7 +241,12 @@ const handleLoyaltyPointsEvent = async (
     }
     // not paid nor cancelled
     return responsePubSub(ECHO_SKIP);
-  } catch (err) {
+  } catch (_err) {
+    const err = _err as ApiError;
+    if (err.statusCode === 404) {
+      logger.warn(err);
+      return null;
+    }
     logger.error(err);
     throw err;
   }
